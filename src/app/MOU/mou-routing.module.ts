4a2ff4caf342eb92319c9215c1283c0eb5fa6f08
Stefani Setiawan <stefani.setiawan@ad-ins.com>
2020-03-11 14:55:21 +0700

    doc signer

---
+++ src/app/MOU/mou-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DocSignerComponent } from './doc-signer/doc-signer.component';
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'DocSigner/Paging',
+        component: DocSignerComponent,
+        data: {
+          title: 'Document Signer Paging'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MouRoutingComponent { }
