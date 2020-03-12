a271f97bd4dbcfb1585fbdcb9d6527e080793580
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-03-12 14:12:29 +0700

    init enviroment

---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  navbarColor: 'transparent',
+  foundationUrl: 'http://r3app.ad-ins.com/foundation', //'https://localhost:5000', //
+  settingUrl: 'http://r3app.ad-ins.com/setting', //'https://localhost:4999',
+  localHostUrl: 'http://R2AppServer/CONFINS',
+  r2AppServerUrl: 'http://R2AppServer/POC/api',
+  losUrl : 'http://r3web.ad-ins.com/LOSR3/pages/login'
+};
+
+
