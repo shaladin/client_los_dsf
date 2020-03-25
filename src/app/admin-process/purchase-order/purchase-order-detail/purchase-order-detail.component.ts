00d95c39537bb1ba8c998ce681e7aa9630d76ad7
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-03-25 17:28:34 +0700

    purchase order detail

--- src/app/admin-process/purchase-order/purchase-order-detail/purchase-order-detail.component.ts
+++ src/app/admin-process/purchase-order/purchase-order-detail/purchase-order-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
 
 @Component({
   selector: 'app-purchase-order-detail',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderDetailComponent implements OnInit {
 
-  constructor() { }
+  arrValue = [];
+  AgrmntId: any;
+  AppId: any;
+  AppAssetId: any;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
+    this.route.queryParams.subscribe(params => {
+      if (params["AgrmntId"] != null) {
+        this.AgrmntId = params["AgrmntId"];
+      }
+      if (params["AppId"] != null) {
+        this.AppId = params["AppId"];
+      }
+      if (params["AppAssetId"] != null) {
+        this.AppAssetId = params["AppAssetId"];
+      }
+    });
+  }
 
   ngOnInit() {
+    this.arrValue.push(this.AgrmntId);
+    var appAssetObj = {
+      AppId: this.AppId
+    }
+    this.http.post(AdInsConstant.GetAppAssetByAppId, appAssetObj).subscribe(
+      (response) => {
+        console.log(response);  
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 
 }
--- src/app/admin-process/purchase-order/purchase-order-paging/purchase-order-paging.component.ts
+++ src/app/admin-process/purchase-order/purchase-order-paging/purchase-order-paging.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UcpagingModule } from '@adins/ucpaging';
+import { environment } from 'environments/environment';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
 
 @Component({
   selector: 'app-purchase-order-paging',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PurchaseOrderPagingComponent implements OnInit {
 
+  inputPagingObj: any;
   constructor() { }
 
   ngOnInit() {
+    this.inputPagingObj = new UcpagingModule();
+    this.inputPagingObj._url = "./assets/ucpaging/searchPurchaseOrder.json";
+    this.inputPagingObj.enviromentUrl = environment.losUrl;
+    this.inputPagingObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
+    this.inputPagingObj.pagingJson = "./assets/ucpaging/searchPurchaseOrder.json";
   }
 
 }
