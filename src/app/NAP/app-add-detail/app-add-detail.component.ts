6eb9650f7818a1d2c3e9ab894a8ca53966d57edb
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-03-27 14:48:12 +0700

    merging

--- src/app/NAP/app-add-detail/app-add-detail.component.ts
+++ src/app/NAP/app-add-detail/app-add-detail.component.ts
@@ -1,69 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
 
 @Component({
   selector: 'app-app-add-detail',
   templateUrl: './app-add-detail.component.html',
-  styleUrls: ['./app-add-detail.component.scss'],
   providers: [NGXToastrService]
 })
 export class AppAddDetailComponent implements OnInit {
 
-  param;
-  constructor(
-    private router: Router, 
-    private route: ActivatedRoute, 
-    private http: HttpClient, 
-    private toastr: NGXToastrService) { 
-    this.route.queryParams.subscribe(params => {
-      if (params["AppId"] != null) {
-        this.param = params["AppId"];
-      }
-    });
-  }
-
-  viewProdMainInfoObj;
-  ngOnInit() {
-    this.viewProdMainInfoObj = "./assets/ucviewgeneric/viewNapAppMainInformation.json";
-  }
-
+  appId : number;
+  viewProdMainInfoObj : string;
   isTab1: boolean = false;
   isTab2: boolean = true;
   isTab3: boolean = false;
   isTab4: boolean = false;
   isTab5: boolean = false;
-  EnterTab(type){
-    if(type == "tab1"){
+
+  constructor(
+    private route: ActivatedRoute, ) {
+    this.route.queryParams.subscribe(params => {
+      if (params["AppId"] != null) {
+        this.appId = params["AppId"];
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.viewProdMainInfoObj = "./assets/ucviewgeneric/viewNapAppMainInformation.json";
+
+
+
+  }
+
+  EnterTab(type) {
+    if (type == "tab1") {
       this.isTab1 = true;
       this.isTab2 = false;
       this.isTab3 = false;
       this.isTab4 = false;
       this.isTab5 = false;
     }
-    if(type == "tab2"){
+    if (type == "tab2") {
       this.isTab1 = false;
       this.isTab2 = true;
       this.isTab3 = false;
       this.isTab4 = false;
       this.isTab5 = false;
     }
-    if(type == "tab3"){
+    if (type == "tab3") {
       this.isTab1 = false;
       this.isTab2 = false;
       this.isTab3 = true;
       this.isTab4 = false;
       this.isTab5 = false;
     }
-    if(type == "tab4"){
+    if (type == "tab4") {
       this.isTab1 = false;
       this.isTab2 = false;
       this.isTab3 = false;
       this.isTab4 = true;
       this.isTab5 = false;
     }
-    if(type == "tab5"){
+    if (type == "tab5") {
       this.isTab1 = false;
       this.isTab2 = false;
       this.isTab3 = false;
