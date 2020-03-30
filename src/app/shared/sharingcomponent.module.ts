a1e0f937990f31aac92778cacbda4d04cad221d4
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-03-30 16:29:46 +0700

    tc

--- src/app/shared/sharingcomponent.module.ts
+++ src/app/shared/sharingcomponent.module.ts
@@ -1,6 +1,6 @@
 
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpModule } from '@angular/http';
@@ -9,6 +9,7 @@ import { UcSubsectionModule } from '@adins/uc-subsection';
 import { UcgridfooterModule } from '@adins/ucgridfooter';
 import { ShowErrorsComponent } from './show-error/show-errors.component';
 import { UcviewgenericModule } from '@adins/ucviewgeneric';
+import { UcShowErrorsModule } from '@adins/uc-show-errors';
 import { AgrMainInfoComponent } from './components/agr-main-info/agr-main-info.component';
 import { TermConditionsComponent } from './components/term-conditions/term-conditions.component';
 
@@ -20,6 +21,7 @@ import { TermConditionsComponent } from './components/term-conditions/term-condi
         TermConditionsComponent
     ],
     imports: [
+        ReactiveFormsModule,
         FormsModule,
         NgbModule,
         RouterModule,
@@ -27,7 +29,8 @@ import { TermConditionsComponent } from './components/term-conditions/term-condi
         CommonModule,
         UcSubsectionModule,
         UcgridfooterModule,
-        UcviewgenericModule
+        UcviewgenericModule,
+        UcShowErrorsModule
     ],
     declarations: [
         ShowErrorsComponent,
