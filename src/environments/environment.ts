02697fd0741e6b97c16f246ce97a7698e3e89d9f
Edward Bala Yansen <edward.by@ad-ins.com>
2020-04-13 14:51:17 +0700

    App Referantor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
   navbarColor: 'transparent',
   losUrl : 'http://localhost:5001',
   // losUrl : 'http://r3app-server.ad-ins.com/LOS',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3'
-  // FoundationR3Url: 'http://localhost:5000'
+  // FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3'
+  FoundationR3Url: 'http://localhost:5000'
 };
 
 
