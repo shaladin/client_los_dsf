dcb04e6ba64d7ce22c20647ccc101a8183db0aff
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-04-13 17:12:03 +0700

    envi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   //losUrl : 'http://localhost:5001',
   FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   FoundationR3Web: 'http://r3web-server/Foundation',
-  losUrl : 'http://r3app-server.ad-ins.com/LOS',
+  losUrl : 'http://r3app-server.ad-ins.com/LOS', 
 };
 
 
