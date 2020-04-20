ef0add9393d3babb3ae65fbc6088662ca680cda4
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-04-20 10:33:16 +0700

    App

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@
 export const environment = {
   production: true,
   navbarColor: 'transparent',
-  //losUrl : 'http://localhost:5001',
+  losUrl : 'http://localhost:5001',
   FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
-   losUrl : 'http://r3app-server.ad-ins.com/LOS',
+  //losUrl: 'http://r3app-server.ad-ins.com/LOS',
   ApprovalR3Url: 'http://r3app-server.ad-ins.com/approval',
   WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3'
 };
