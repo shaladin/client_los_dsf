046298b6e6fa2548ddce933dcff4230cf22f4a8d
Hartono Rachmat <hartono.rachmat@ad-ins.com>
2020-04-20 15:32:43 +0700

    perbaikan mou

--- src/app/MOU/mou-customer-request/mou-customer-request.component.ts
+++ src/app/MOU/mou-customer-request/mou-customer-request.component.ts
@@ -26,13 +26,6 @@ export class MouCustomerRequestComponent implements OnInit {
     this.inputPagingObj.apiQryPaging = "/Generic/GetPagingObjectBySQL";
     this.inputPagingObj.deleteUrl = "";
     this.inputPagingObj.pagingJson = "./assets/ucpaging/searchMouCustomerRequest.json";
-    var criteriaList = new Array<CriteriaObj>();
-    var criteriaObj = new CriteriaObj();
-    criteriaObj.restriction = AdInsConstant.RestrictionEq;
-    criteriaObj.propName = 'MOU_STAT';
-    criteriaObj.value = 'NEW';
-    criteriaList.push(criteriaObj);
-    this.inputPagingObj.addCritInput = criteriaList;
   }
 
 }
