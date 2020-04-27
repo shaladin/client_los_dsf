faea9444819f8c3292fb379645295e3a9efb2f8d
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-04-27 09:24:15 +0700

    Fix ReturnHandlingExecNotes duplicate

--- src/app/shared/model/ReturnHandling/ReturnHandlingDObj.Model.ts
+++ src/app/shared/model/ReturnHandling/ReturnHandlingDObj.Model.ts
@@ -1,18 +1,17 @@
 export class ReturnHandlingDObj {
-    ReturnHandlingDId: number;
-    ReturnHandlingHId: number;
-    MrReturnTaskCode: string;
-    ReturnTaskName: string;
-    ReturnStat: string;
-    ReturnStatName: string;
-    ReturnHandlingNotes: string;
-    ReturnHandlingExecNotes : string;
-    WfTaskListId: number;
-    AppId: number;
+  ReturnHandlingDId: number;
+  ReturnHandlingHId: number;
+  MrReturnTaskCode: string;
+  ReturnTaskName: string;
+  ReturnStat: string;
+  ReturnStatName: string;
+  ReturnHandlingNotes: string;
+  ReturnHandlingExecNotes: string;
+  WfTaskListId: number;
+  AppId: number;
   RowVersion: any;
-  ReturnHandlingExecNotes: any;
 
-    constructor() {
-        this.RowVersion = ""; 
-    }
+  constructor() {
+    this.RowVersion = "";
+  }
 }
