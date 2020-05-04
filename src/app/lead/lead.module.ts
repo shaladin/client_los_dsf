6b5d3f6d972309360f8e4646e5940d69f39748f2
Stefani Setiawan <stefani.setiawan@ad-ins.com>
2020-05-04 17:58:37 +0700

    fixing lead input

--- src/app/lead/lead.module.ts
+++ src/app/lead/lead.module.ts
@@ -49,6 +49,7 @@ import { LeadUpdateLeadDataComponent } from "./lead-update/lead-update-lead-data
 import { NgxCurrencyModule } from "ngx-currency";
 import { CustJobDataComponent } from "app/NEW-NAP/sharing-component/input-nap-component/customer-data/component/job-data/cust-job-data.component";
 import { CustSocmedComponent } from "app/NEW-NAP/sharing-component/input-nap-component/customer-data/component/socmed/cust-socmed.component";
+import { UcinputnumberModule } from "@adins/ucinputnumber";
 export const customCurrencyMaskConfig = {     
   align: "left",     
   allowNegative: true,     
@@ -83,7 +84,8 @@ export const customCurrencyMaskConfig = {
     SharingComponentModule,
     ArchwizardModule,
     MatRadioModule,
-    NgxCurrencyModule.forRoot(customCurrencyMaskConfig)
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig),
+    UcinputnumberModule
   ],
   declarations: [
     MainInfoComponent,
