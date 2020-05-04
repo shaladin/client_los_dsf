c41b6160f8c610a96b792cf0d77c36f0f60c7ee6
Ifvan Agustrianto <ifvan.agustrianto@msn.com>
2020-05-04 15:05:23 +0700

    fix asset data

--- src/app/shared/AdInstConstant.ts
+++ src/app/shared/AdInstConstant.ts
@@ -559,9 +559,9 @@ export class AdInsConstant {
   public static GetListVendorEmpByVendorId = environment.FoundationR3Url + "/VendorEmp/GetListVendorEmpByVendorId";
   public static GetListVendorEmpByVendorIdAndPositionCodes = environment.FoundationR3Url + "/VendorEmp/GetListVendorEmpByVendorIdAndPositionCodes";
   public static GetVendorEmpSupervisorByVendorEmpId = environment.FoundationR3Url + "/VendorEmp/GetVendorEmpSupervisorByVendorEmpId";
-  public static GetVendorEmpByVendorIdVendorEmpNo = environment.losUrl + "/VendorEmp/GetVendorEmpByVendorIdVendorEmpNo";
+  public static GetVendorEmpByVendorIdVendorEmpNo = environment.FoundationR3Url + "/VendorEmp/GetVendorEmpByVendorIdVendorEmpNo";
   public static GetListVendorByCategoryCodeAndOfficeCode = environment.FoundationR3Url + "/Vendor/GetListVendorByCategoryCodeAndOfficeCode";
-  public static GetVendorForLookup = "http://localhost:5000" + "/Vendor/GetVendorForLookup";
+  public static GetVendorForLookup = environment.FoundationR3Url + "/Vendor/GetVendorForLookup";
 
 
   // VendorEmp
