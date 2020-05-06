cef85da10dd0f9d61136daf0c3faca511e452860
Ivan Keefe <ivan.keefe@ad-ins.com>
2020-05-06 16:46:35 +0700

    View App Fraud Verif

--- src/app/NEW-NAP/sharing-component/view-app-component/view-app-component.module.ts
+++ src/app/NEW-NAP/sharing-component/view-app-component/view-app-component.module.ts
@@ -21,6 +21,7 @@ import { ViewFinancialComponent } from "./view-financial/view-financial.componen
 import { AppInsuranceComponent } from './app-insurance/app-insurance.component';
 import { AppInsuranceDetailComponent } from './app-insurance/app-insurance-detail/app-insurance-detail.component';
 import { ViewSharingComponentModule } from "./view-sharing-component.module";
+import { ViewAppFraudVerificationComponent } from './view-app-fraud-verification/view-app-fraud-verification.component';
 
 @NgModule({
   exports: [
@@ -57,7 +58,8 @@ import { ViewSharingComponentModule } from "./view-sharing-component.module";
     AppAssetDataDetailComponent,
     ViewFinancialComponent,
     AppInsuranceComponent,
-    AppInsuranceDetailComponent
+    AppInsuranceDetailComponent,
+    ViewAppFraudVerificationComponent    
   ],
   providers: [
     NGXToastrService
