d12a90aa246797e58b862f12f5ebfdd7187be1d1
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-05-08 15:45:06 +0700

    FL4W Tab

--- src/app/NEW-NAP/FL4W/input-nap.module.ts
+++ src/app/NEW-NAP/FL4W/input-nap.module.ts
@@ -13,7 +13,28 @@ import { SharingComponentModule } from "app/shared/sharingcomponent.module";
 import { UcinputnumberModule } from "@adins/ucinputnumber"; 
 import { NapAddComponent } from './nap-add/nap-add.component';
 import { NapPagingComponent } from './nap-paging/nap-paging.component';
-import { NapViewComponent } from './nap-view/nap-view.component';
+import { NapViewComponent } from './nap-view/nap-view.component';  
+import { CustomerDataComponent } from "./nap-tab/customer-data/customer-data.component";
+import { CustCompanyFinancialDataComponent } from "./nap-tab/customer-data/component/company-financial-data/cust-company-financial-data.component";
+import { CustUcaddressComponent } from "./nap-tab/customer-data/component/address/address.component";
+import { CustPersonalMainDataComponent } from "./nap-tab/customer-data/component/personal-main-data/cust-personal-main-data.component";
+import { CustPersonalContactInformationComponent } from "./nap-tab/customer-data/component/personal-contact-information/cust-personal-contact-information.component";
+import { CustPersonalFinancialDataComponent } from "./nap-tab/customer-data/component/personal-financial-data/cust-personal-financial-data.component";
+import { CustBankAccountComponent } from "./nap-tab/customer-data/component/bank-account/cust-bank-account.component";
+import { CustJobDataComponent } from "./nap-tab/customer-data/component/job-data/cust-job-data.component";
+import { CustSocmedComponent } from "./nap-tab/customer-data/component/socmed/cust-socmed.component";
+import { CustGrpMemberComponent } from "./nap-tab/customer-data/component/cust-grp-member/cust-grp-member.component";
+import { CustCompanyMainDataComponent } from "./nap-tab/customer-data/component/company-main-data/cust-company-main-data.component";
+import { CustShareholderComponent } from "./nap-tab/customer-data/component/shareholder/cust-shareholder.component";
+import { CustCompanyContactInformationComponent } from "./nap-tab/customer-data/component/company-contact-information/cust-company-contact-information.component";
+import { CustLegalDocComponent } from "./nap-tab/customer-data/component/legal-doc/cust-legal-doc.component";
+import { GuarantorComponent } from "../sharing-component/view-app-component/guarantor/guarantor.component";
+import { GuarantorPagingComponent } from "./nap-tab/guarantor-data/guarantor-paging/guarantor-paging.component";
+import { GuarantorPersonalComponent } from "./nap-tab/guarantor-data/guarantor-personal/guarantor-personal.component";
+import { GuarantorCompanyComponent } from "./nap-tab/guarantor-data/guarantor-company/guarantor-company.component";
+import { ReferantorDataComponent } from "./nap-tab/referantor-data/referantor-data.component";
+import { MatCheckboxModule, MatRadioModule, MatSelectModule } from "@angular/material";
+import { NgxCurrencyModule } from "ngx-currency";
 
 @NgModule({
   imports: [
@@ -27,13 +48,36 @@ import { NapViewComponent } from './nap-view/nap-view.component';
     ReactiveFormsModule,
     FormsModule,
     SharingComponentModule,
-    UcinputnumberModule
+    UcinputnumberModule, 
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig)
   ],
   declarations: [
     DummyComponent, 
     NapAddComponent,
     NapPagingComponent,
-    NapViewComponent
+    NapViewComponent, 
+    CustomerDataComponent,
+    CustCompanyFinancialDataComponent,
+    CustUcaddressComponent,
+    CustPersonalMainDataComponent,
+    CustPersonalContactInformationComponent,
+    CustPersonalFinancialDataComponent,
+    CustBankAccountComponent,
+    CustJobDataComponent,
+    CustSocmedComponent,
+    CustGrpMemberComponent,
+    CustCompanyMainDataComponent,
+    CustShareholderComponent,
+    CustCompanyContactInformationComponent,
+    CustLegalDocComponent,
+    GuarantorComponent,
+    GuarantorPagingComponent,
+    GuarantorPersonalComponent,
+    GuarantorCompanyComponent,
+    ReferantorDataComponent,  
   ],
   providers: [
     NGXToastrService
