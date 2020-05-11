f9ea1bd7aed7720c06894fb5e0c8c2e24e1df6da
Ifvan Agustrianto <ifvan.agustrianto@msn.com>
2020-05-11 13:59:26 +0700

    tambah local storage lob

--- src/app/NEW-NAP/business-process/credit-process/phone-verification/phone-verification-paging/phone-verification-paging.component.ts
+++ src/app/NEW-NAP/business-process/credit-process/phone-verification/phone-verification-paging/phone-verification-paging.component.ts
@@ -11,14 +11,17 @@ import { ActivatedRoute } from "@angular/router";
   templateUrl: "./phone-verification-paging.component.html",
   providers: [DecimalPipe]
 })
+
 export class PhoneVerificationPagingComponent implements OnInit {
   inputPagingObj: any;
   arrCrit: any;
+  lobCode : string;
+
   constructor(private route: ActivatedRoute) { 
     this.route.queryParams.subscribe(params => {
       if (params['LobCode'] != null) {
-        var lobCode = params['LobCode'];
-        localStorage.setItem("LobCode",lobCode);
+        this.lobCode = params['LobCode'];
+        localStorage.setItem("LobCode",this.lobCode);
       }
     });
   }
@@ -30,6 +33,13 @@ export class PhoneVerificationPagingComponent implements OnInit {
     this.inputPagingObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
     this.inputPagingObj.pagingJson = "./assets/ucpaging/searchAppPhoneVerif.json";
 
-    
+    this.arrCrit = new Array();
+    var critObj = new CriteriaObj();
+    critObj.restriction = AdInsConstant.RestrictionLike;
+    critObj.propName = 'RL.BL_CODE';
+    critObj.value = this.lobCode;
+    this.arrCrit.push(critObj);
+    this.inputPagingObj.addCritInput = this.arrCrit;
+
   }
 }
