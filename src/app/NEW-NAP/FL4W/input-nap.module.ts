2066587043923d23ff56227e754c918e13c53135
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-05-11 18:01:47 +0700

    FL4W

--- src/app/NEW-NAP/FL4W/input-nap.module.ts
+++ src/app/NEW-NAP/FL4W/input-nap.module.ts
@@ -13,13 +13,16 @@ import { UcinputnumberModule } from "@adins/ucinputnumber";
 import { NapAddComponent } from './nap-add/nap-add.component';
 import { NapPagingComponent } from './nap-paging/nap-paging.component';
 import { NapViewComponent } from './nap-view/nap-view.component';   
-import { MatCheckboxModule, MatRadioModule, MatSelectModule } from "@angular/material";
+import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatTabsModule } from "@angular/material";
 import { NgxCurrencyModule } from "ngx-currency";
 import { customCurrencyMaskConfig } from "app/MOU/mou.module"; 
 import { NapAddDetailComponent } from './nap-add-detail/nap-add-detail.component';
 import { NapTabComponentModule } from "./nap-tab/nap-tab.module";
 import { ViewMainInfoComponentModule } from "../sharing-component/view-main-info-component/view-main-info-component.module";
 import { ViewAppComponentModule } from "../sharing-component/view-app-component/view-app-component.module";
+import { AppMainInfoComponent } from "../sharing-component/view-main-info-component/app-main-info/app-main-info.component";
+import { ViewAppCustDataPersonalComponent } from "../sharing-component/view-app-component/view-app-cust-data-personal/view-app-cust-data-personal.component";
+import { ViewAppCustDataCompanyComponent } from "../sharing-component/view-app-component/view-app-cust-data-company/view-app-cust-data-company.component";
  
 
 @NgModule({
@@ -38,8 +41,7 @@ import { ViewAppComponentModule } from "../sharing-component/view-app-component/
     MatRadioModule,
     MatSelectModule,
     NapTabComponentModule,
-    ViewMainInfoComponentModule,
-    ViewAppComponentModule,
+    MatTabsModule,
     NgxCurrencyModule.forRoot(customCurrencyMaskConfig)
   ],
   declarations: [
@@ -48,7 +50,9 @@ import { ViewAppComponentModule } from "../sharing-component/view-app-component/
     NapPagingComponent,
     NapViewComponent,  
     NapAddDetailComponent, 
-   
+    AppMainInfoComponent,
+    ViewAppCustDataPersonalComponent,
+    ViewAppCustDataCompanyComponent
   ],
   providers: [
     NGXToastrService
