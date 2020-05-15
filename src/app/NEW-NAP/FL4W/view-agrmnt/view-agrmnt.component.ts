3952f84bf5c64b310ab4fd4424f3e70ed0651ad9
Randy Khengdy <randy.khengdy@ad-ins.com>
2020-05-15 17:28:13 +0700

    Agr

--- src/app/NEW-NAP/FL4W/view-agrmnt/view-agrmnt.component.ts
+++ src/app/NEW-NAP/FL4W/view-agrmnt/view-agrmnt.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-view-agrmnt',
-  templateUrl: './view-agrmnt.component.html',
-  styleUrls: ['./view-agrmnt.component.scss']
+  templateUrl: './view-agrmnt.component.html' 
 })
 export class ViewAgrmntComponent implements OnInit {
 
   constructor() { }
-
+  viewAgrMainInfo : string;
   ngOnInit() {
+    this.viewAgrMainInfo = "./assets/ucviewgeneric/viewAgrMainInfo.json";
   }
 
 }
