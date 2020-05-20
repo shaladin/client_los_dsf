98e6fab9bd92908bf9ae814f502881262c9bfb26
Edward Bala Yansen <edward.by@ad-ins.com>
2020-05-20 10:01:56 +0700

    app data

--- src/app/shared/AdInstConstant.ts
+++ src/app/shared/AdInstConstant.ts
@@ -436,7 +436,7 @@ export class AdInsConstant {
   public static RefMasterTypeCodeCustType = "CUST_TYPE";
   public static RefMasterTypeCodeSlsRecom = "SLS_RECOM";
   public static RefMasterTypeCodeWOP = "WOP";
-  public static RefMasterTypeCodeInstSchm = "INST_SCHM";
+  public static RefMasterTypeCodeInstSchm = "INSTSCHM";
   public static RefMasterTypeCodeCustNotifyOpt = "CUST_NOTIF_OPT";
   public static RefMasterTypeCodeFirstInstType = "FIRST_INST_TYPE";
   public static RefMasterTypeCodeInterestType = "INTRSTTYPE";
