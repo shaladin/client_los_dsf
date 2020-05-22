a566d5dd86ce791db33a49a3f28b7069c79e5e18
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-05-22 10:17:45 +0700

    perbaikan url navigate

--- src/app/NEW-NAP/CF4W/nap-add/nap-add.component.ts
+++ src/app/NEW-NAP/CF4W/nap-add/nap-add.component.ts
@@ -212,7 +212,7 @@ export class NapAddComponent implements OnInit {
       (response) => {
         console.log(response);
         this.toastr.successMessage(response["message"]);
-        this.router.navigate(["Nap/ConsumerFinance/Add/Detail"], { queryParams: { "AppId": response["AppId"] } });
+        this.router.navigate(["Nap/ConsumerFinance/InputNap/Add/Detail"], { queryParams: { "AppId": response["AppId"] } });
       },
       (error) => {
         console.log(error);
