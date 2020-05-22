d65dd179e7410d28ebe74e7bcdfb633d98ee00b9
Reynard Winoto Fatimah <reynard.wf@ad-ins.com>
2020-05-22 17:51:58 +0700

    commit

--- src/app/NEW-NAP/sharing-component/input-nap-component/input-nap-component.module.ts
+++ src/app/NEW-NAP/sharing-component/input-nap-component/input-nap-component.module.ts
@@ -58,6 +58,7 @@ import { SchmEvenPrincipalFctrComponent } from "./financial-data-fctr/component/
 import { SingleInstFctrComponent } from "./financial-data-fctr/component/single-installment/single-inst-fctr.component";
 import { FeeFctrComponent } from "./financial-data-fctr/component/fee/fee-fctr.component";
 import { CollateralFctrComponent } from "./multi-asset-data/collateral-fctr/collateral-fctr.component";
+import { InvoiceDataComponent } from "./invoice-data/invoice-data.component";
 
 export const customCurrencyMaskConfig = {     
   align: "right",     
@@ -121,7 +122,8 @@ export const customCurrencyMaskConfig = {
     SingleInstFctrComponent,
     SchmRegulerFixFctrComponent,
     FeeFctrComponent,
-    CollateralFctrComponent
+    CollateralFctrComponent,
+    InvoiceDataComponent
     // MultiAssetLeasingComponent,
     // AssetLeasingPagingComponent,
     // AssetLeasingAddEditComponent,
@@ -185,7 +187,8 @@ export const customCurrencyMaskConfig = {
     SingleInstFctrComponent,
     SchmRegulerFixFctrComponent,
     FeeFctrComponent,
-    CollateralFctrComponent
+    CollateralFctrComponent,
+    InvoiceDataComponent
     // MultiAssetLeasingComponent,
     // AssetLeasingPagingComponent,
     // AssetLeasingAddEditComponent,
