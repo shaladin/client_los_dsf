0529ed803b3f4bd01ecd5e72c778ccac7fb4bf28
Ifvan Agustrianto <ifvan@ifvanagustrianto.co.id>
2020-05-31 16:18:53 +0700

    ubah json search return handling

--- src/app/NEW-NAP/business-process/additional-process/return-handling/return-handling-com-rsvfund-paging/return-handling-com-rsvfund-paging.component.ts
+++ src/app/NEW-NAP/business-process/additional-process/return-handling/return-handling-com-rsvfund-paging/return-handling-com-rsvfund-paging.component.ts
@@ -3,6 +3,8 @@ import { UcPagingObj } from 'app/shared/model/UcPagingObj.Model';
 import { environment } from 'environments/environment';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
+import { HttpClient } from '@angular/common/http';
+import { CenterGrpOfficeMbrObj } from 'app/shared/model/RefOffice/CenterGrpOfficeMbrObj.Model';
 
 @Component({
   selector: 'app-return-handling-com-rsvfund-paging',
@@ -11,27 +13,56 @@ import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 })
 export class ReturnHandlingComRsvfundPagingComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  userAccess;
   inputPagingObj;
   ngOnInit() {
+    this.userAccess = JSON.parse(localStorage.getItem("UserAccess"));
+
     this.inputPagingObj = new UcPagingObj();
-    this.inputPagingObj._url="./assets/ucpaging/searchReturnHandlingCommission.json";
+    this.inputPagingObj._url = "./assets/ucpaging/searchReturnHandlingCommission.json";
     this.inputPagingObj.enviromentUrl = environment.losUrl;
     this.inputPagingObj.apiQryPaging = AdInsConstant.GetPagingObjectBySQL;
     this.inputPagingObj.pagingJson = "./assets/ucpaging/searchReturnHandlingCommission.json";
 
-    var currentUserContext = JSON.parse(localStorage.getItem("UserAccess"));
-    var addCrit = new CriteriaObj();
-    addCrit.DataType = 'text';
-    addCrit.propName = 'WTL.USERNAME';
-    addCrit.restriction = AdInsConstant.RestrictionIn;
-    var arrayString = new Array<string>();
-    arrayString.push(currentUserContext["UserName"]);
-    arrayString.push("");
-    addCrit.listValue = arrayString;
+    this.inputPagingObj.addCritInput = this.ActAndOfficeCriteria();
+  }
 
-    this.inputPagingObj.addCritInput.push(addCrit);
+  ActAndOfficeCriteria(): Array<CriteriaObj> {
+    var critObjs: Array<CriteriaObj> = new Array<CriteriaObj>();
+
+    var critObj = new CriteriaObj();
+    critObj.restriction = AdInsConstant.RestrictionLike;
+    critObj.propName = 'WTL.ACT_CODE';
+    critObj.value = "EDIT_COM_RSV_FND";
+    critObjs.push(critObj);
+
+    critObj = new CriteriaObj();
+    critObj.restriction = AdInsConstant.RestrictionIn;
+    if (this.userAccess.MrOfficeTypeCode != AdInsConstant.CENTER_GROUP_CODE) {
+      critObj.propName = 'a.ORI_OFFICE_CODE';
+      critObj.listValue = [this.userAccess.OfficeCode];
+    } else {
+      critObj.propName = 'a.ORI_OFFICE_CODE';
+      var obj = { CenterGrpCode: AdInsConstant.CENTER_GROUP_CODE };
+      this.http.post(AdInsConstant.GetListCenterGrpMemberByCenterGrpCode, obj).subscribe(
+        (response) => {
+          var CenterGrpOfficeMbrObjs: Array<CenterGrpOfficeMbrObj> = response["ListCenterGrpOfficeMbr"];
+
+          var listDataTemp = new Array();
+          for (var i = 0; i < CenterGrpOfficeMbrObjs.length; i++) {
+            listDataTemp.push(CenterGrpOfficeMbrObjs[i].RefOfficeCode);
+          }
+          critObj.listValue = listDataTemp;
+        },
+        (error) => {
+          console.log(error);
+        }
+      )
+    }
+    critObjs.push(critObj);
+
+    return critObjs;
   }
 
 }
