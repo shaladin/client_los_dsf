c025a2b82307d929e3f08170837f22631d1253ce
Hartono Rachmat <hartono.rachmat@ad-ins.com>
2020-06-02 17:42:08 +0700

    update NAP Add dan Sidebar

--- src/app/NEW-NAP/FL4W/nap-add-detail/nap-add-detail.component.ts
+++ src/app/NEW-NAP/FL4W/nap-add-detail/nap-add-detail.component.ts
@@ -62,6 +62,7 @@ export class NapAddDetailComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.ClaimTask();
     this.viewProdMainInfoObj = "./assets/ucviewgeneric/viewNapAppFL4WMainInformation.json";
     this.NapObj.AppId = this.appId;
     this.http.post(AdInsConstant.GetAppById, this.NapObj).subscribe(
@@ -216,5 +217,18 @@ export class NapAddDetailComponent implements OnInit {
         }
       )
     }
+  }
+
+  ClaimTask(){
+    var currentUserContext = JSON.parse(localStorage.getItem("UserAccess"));
+    var wfClaimObj = new AppObj();
+    wfClaimObj.AppId = this.appId;
+    wfClaimObj.Username = currentUserContext["UserName"];
+    wfClaimObj.WfTaskListId = this.wfTaskListId;
+
+    this.http.post(AdInsConstant.ClaimTaskNap, wfClaimObj).subscribe(
+      () => {
+    
+      });
   }
 }
