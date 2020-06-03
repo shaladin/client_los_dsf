9c179dafc9ce7c883c46d35972dc5643575789a4
Edward Bala Yansen <edward.by@ad-ins.com>
2020-06-03 09:13:31 +0700

    app model bug done kurang data MaturityDt

--- src/app/NEW-NAP/sharing-component/input-nap-component/application-data/application-data.component.ts
+++ src/app/NEW-NAP/sharing-component/input-nap-component/application-data/application-data.component.ts
@@ -155,7 +155,9 @@ export class ApplicationDataComponent implements OnInit {
     }
     this.http.post(AdInsConstant.GetListAppCross, obj).subscribe(
       (response) => {
+        console.log(response);
         this.resultCrossApp = response["ReturnObject"];
+        console.log(this.resultCrossApp);
         for (var i = 0; i < this.resultCrossApp.length; i++) {
           this.ListCrossAppObj["result"].push(this.resultCrossApp[i].CrossAgrmntNo);
         }
@@ -447,10 +449,11 @@ export class ApplicationDataComponent implements OnInit {
 
   GetListAppCrossValue() {
     var arr = [];
-    var temp = new NapAppCrossObj();
     for (var i = 0; i < this.resultCrossApp.length; i++) {
-      if (this.resultCrossApp[i].AppCrossId == null) {
-        temp.AppId = this.resultCrossApp[i].appId;
+      if (this.resultCrossApp[i].AppCrossId == 0) {
+        console.log(this.resultCrossApp[i]);
+        var temp = new NapAppCrossObj();
+        temp.AppId = this.appId;
         temp.CrossAgrmntNo = this.resultCrossApp[i].CrossAgrmntNo;
         temp.CrossAppNo = this.resultCrossApp[i].CrossAppNo;
         temp.CustName = this.resultCrossApp[i].CustName;
@@ -483,7 +486,8 @@ export class ApplicationDataComponent implements OnInit {
       appFinData: tempAppFindDataObj,
       RowVersion: ""
     };
-
+    console.log(obj);
+    console.log("test");
     this.http.post(url, obj).subscribe(
       (response) => {
         console.log(response);
@@ -523,12 +527,19 @@ export class ApplicationDataComponent implements OnInit {
     this.Open(contentCrossApp);
   }
 
-  resultCrossApp = [];
+  resultCrossApp: Array<NapAppCrossObj> = new Array<NapAppCrossObj>();
   GetDataTemp(ev) {
     for (let i of ev) {
-      this.resultCrossApp.push(i);
+      var tempCrossApp = new NapAppCrossObj();
+      tempCrossApp.CrossAgrmntNo=i.AgrmntNo;
+      tempCrossApp.CrossAppNo=i.AppNo;
+      tempCrossApp.CustName=i.CustName;
+      tempCrossApp.ContractStat=i.AgrmntStat;
+      tempCrossApp.MaturityDt;
+      this.resultCrossApp.push(tempCrossApp);
       this.ListCrossAppObj["result"].push(i.AgrmntNo);
     }
+    console.log(this.resultCrossApp);
   }
 
   DeleteCrossApp(idx) {
