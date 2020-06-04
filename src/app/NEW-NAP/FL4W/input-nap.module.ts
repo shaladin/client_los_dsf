b7171faea20c88c494dc6a495dff6a238409e1d6
Hartono Rachmat <hartono.rachmat@ad-ins.com>
2020-06-04 15:55:52 +0700

    perbaikan input nap

--- src/app/NEW-NAP/FL4W/input-nap.module.ts
+++ src/app/NEW-NAP/FL4W/input-nap.module.ts
@@ -88,7 +88,6 @@ export const customCurrencyMaskConfig = {
     NapPagingComponent,
     NapViewComponent,  
     NapAddDetailComponent, 
-    AppMainInfoComponent,
     ViewAgrmntComponent, 
     TabCommissionComponent,
     ListDataCommissionComponent,
