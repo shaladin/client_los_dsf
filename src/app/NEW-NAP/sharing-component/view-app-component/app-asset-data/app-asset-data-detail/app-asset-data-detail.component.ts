481e8c462e72caaec0e81ee5dda86de6c418f2cc
Stefani Setiawan <stefani.setiawan@ad-ins.com>
2020-06-05 11:28:51 +0700

    fixing view multi asset detail

--- src/app/NEW-NAP/sharing-component/view-app-component/app-asset-data/app-asset-data-detail/app-asset-data-detail.component.ts
+++ src/app/NEW-NAP/sharing-component/view-app-component/app-asset-data/app-asset-data-detail/app-asset-data-detail.component.ts
@@ -37,10 +37,10 @@ export class AppAssetDataDetailComponent implements OnInit {
         this.appAssetSupplEmp = response[1];
         this.appCollateralRegistration = response[2];
 
-        console.log("assetdetail")
-        console.log(this.appAsset)
-        console.log(this.appAssetSupplEmp)
-        console.log(this.appCollateralRegistration)
+        // console.log("assetdetail")
+        // console.log(this.appAsset)
+        // console.log(this.appAssetSupplEmp.ReturnObject)
+        // console.log(this.appCollateralRegistration)
 
         for (const item of this.appAssetSupplEmp.ReturnObject) {
           if(item.MrSupplEmpPositionCode == AdInsConstant.SALES_JOB_CODE){
