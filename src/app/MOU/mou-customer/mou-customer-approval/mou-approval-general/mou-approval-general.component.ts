cea4e31e189d262902101d5f4e547bae80dd1686
Stefani Setiawan <stefani.setiawan@ad-ins.com>
2020-06-19 13:39:53 +0700

    show cancel di mou approval

--- src/app/MOU/mou-customer/mou-customer-approval/mou-approval-general/mou-approval-general.component.ts
+++ src/app/MOU/mou-customer/mou-customer-approval/mou-approval-general/mou-approval-general.component.ts
@@ -53,4 +53,9 @@ export class MouApprovalGeneralComponent implements OnInit {
     this.toastr.successMessage("Success");
     this.router.navigate(["/Mou/Cust/Approval"]);
   }
+
+  onCancelClick()
+  {
+    this.router.navigate(["/Mou/Cust/Approval"]);
+  }
 }
