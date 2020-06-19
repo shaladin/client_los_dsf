936a27eb7abd1697425eb3bdb0b463f03b1a1787
Ardi Gunawan <ardi.gunawan@ad-ins.com>
2020-06-19 18:15:50 +0700

    Fix bug Mou Add all to temp, bug fix AgrmntView

--- src/app/MOU/mou-customer/mou-review/mou-review-factoring/mou-review-factoring.component.ts
+++ src/app/MOU/mou-customer/mou-review/mou-review-factoring/mou-review-factoring.component.ts
@@ -140,7 +140,8 @@ export class MouReviewFactoringComponent implements OnInit {
 
     var submitMouReviewObj = {
       WfTaskListId: this.WfTaskListId,
-      MouCust: this.mouCustObj, Rfa: this.rfaInfoObj
+      MouCust: this.mouCustObj, Rfa: this.rfaInfoObj,
+      PlafondAmt: this.PlafondAmt
     }
     this.http.post(AdInsConstant.SubmitMouReview, submitMouReviewObj).subscribe(
       (response) => {
