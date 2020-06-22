df01f9a5467f48cef7d8e7e7cf3c60d30b610747
Ivan Keefe <ivan.keefe@ad-ins.com>
2020-06-22 11:31:58 +0700

    Add Residual Value Amt FL4W Fin Data

--- src/app/NEW-NAP/FL4W/input-nap-FL4W/financial-data-FL4W/financial-data-FL4W.component.ts
+++ src/app/NEW-NAP/FL4W/input-nap-FL4W/financial-data-FL4W/financial-data-FL4W.component.ts
@@ -78,6 +78,7 @@ export class FinancialDataFL4WComponent implements OnInit {
         SupplEffectiveRatePrcnt: 0,
         SupplFlatRatePrcnt: 0,
         DiffRateAmt: 0,
+        ResidualValueAmt: 0,
 
         TotalInterestAmt: 0,
         TotalAR: 0,
