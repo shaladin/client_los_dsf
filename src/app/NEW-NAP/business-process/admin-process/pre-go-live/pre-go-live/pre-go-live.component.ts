47e4981e80d3ba694bd9b6e13bb0c56456e309fa
Jacksen <jacksen@ad-ins.com>
2020-06-24 16:30:40 +0700

    perubahan if pada ngOnInit

--- src/app/NEW-NAP/business-process/admin-process/pre-go-live/pre-go-live/pre-go-live.component.ts
+++ src/app/NEW-NAP/business-process/admin-process/pre-go-live/pre-go-live/pre-go-live.component.ts
@@ -78,10 +78,11 @@ export class PreGoLiveComponent implements OnInit {
             type: 'task',
             refId: this.ListRfaLogObj[i].RfaNo
           };
-          if(this.ListRfaLogObj[i].ApvStat == "ApproveFinal"){
+          if(this.ListRfaLogObj[i].ApvStat != "RejectFinal"){
             this.IsCheckedAll = false;
             this.hasApproveFinal = true;
           }
+
         }
       },
       (error) => {
@@ -115,9 +116,8 @@ export class PreGoLiveComponent implements OnInit {
       window.open( link, "_blank");
     }
     if (ev.Key == "customer") {
-      console.log("customer")
-          var link = environment.FoundationR3Web + "/Customer/CustomerView/Page?CustId=" + ev.ViewObj.AppCustId; 
-          window.open(link, '_blank');
+          var link = environment.FoundationR3Web + "/Customer/CustomerView/Page?CustId=" + ev.ViewObj.AppCustId + "&Token=" + this.token;
+          window.open(link, "_blank");
     }
   }
 
