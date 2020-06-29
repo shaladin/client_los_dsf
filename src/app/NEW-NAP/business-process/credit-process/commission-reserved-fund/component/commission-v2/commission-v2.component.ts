e935d3089897a74a1cb1c2600f72cbc057a74fc4
Edward Bala Yansen <edward.by@ad-ins.com>
2020-06-29 13:03:06 +0700

    commission ver 2

---
+++ src/app/NEW-NAP/business-process/credit-process/commission-reserved-fund/component/commission-v2/commission-v2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-commission-v2',
+  templateUrl: './commission-v2.component.html',
+  styleUrls: ['./commission-v2.component.scss']
+})
+export class CommissionV2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/NEW-NAP/business-process/credit-process/commission-reserved-fund/component/commission-v2/form-commission-generate/form-commission-generate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-commission-generate',
+  templateUrl: './form-commission-generate.component.html',
+  styleUrls: ['./form-commission-generate.component.scss']
+})
+export class FormCommissionGenerateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
