726cd1f0f13f953ab98c2f424829bb090526c4fa
Andre Sucipto <andre.sucipto@ad-ins.com>
2020-06-29 09:31:57 +0700

    SITCFRTHREE-566, perbaikan pada paging agreement application cancellation

--- src/app/NEW-NAP/business-process/admin-process/application-agreement-cancellation/application-agreement-cancellation-paging/application-agreement-cancellation-paging.component.ts
+++ src/app/NEW-NAP/business-process/admin-process/application-agreement-cancellation/application-agreement-cancellation-paging/application-agreement-cancellation-paging.component.ts
@@ -3,6 +3,7 @@ import { UcPagingObj } from 'app/shared/model/UcPagingObj.Model';
 import { environment } from 'environments/environment';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-application-agreement-cancellation-paging',
@@ -11,15 +12,15 @@ import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 })
 export class ApplicationAgreementCancellationPagingComponent implements OnInit {
   inputPagingObj: any;
-
-  constructor() { }
+  token: any = localStorage.getItem("Token");
+  constructor(private router: Router) { }
 
   ngOnInit() {
 
     var critInputAppStatNotCancel = new CriteriaObj();
     critInputAppStatNotCancel.propName = "ap.APP_STAT";
     critInputAppStatNotCancel.restriction = AdInsConstant.RestrictionNeq;
-    critInputAppStatNotCancel.value = "CANCEL";
+    critInputAppStatNotCancel.value = AdInsConstant.AppStatCancel;
 
     this.inputPagingObj = new UcPagingObj();
     this.inputPagingObj._url = "./assets/ucpaging/searchApplicationAgreementCancellation.json";
@@ -29,6 +30,12 @@ export class ApplicationAgreementCancellationPagingComponent implements OnInit {
     this.inputPagingObj.addCritInput = new Array();
 
     this.inputPagingObj.addCritInput.push(critInputAppStatNotCancel);
-    
+
+  }
+  GetCallBack(ev: any) {
+    if (ev.Key == "ViewProdOffering") {
+      var link = environment.FoundationR3Web + "/Product/OfferingView?prodOfferingHId=0&prodOfferingCode=" + ev.RowObj.ProdOfferingCode + "&prodOfferingVersion=" + ev.RowObj.ProdOfferingVersion + "&Token=" + this.token;
+      this.router.navigate([]).then(result => { window.open(link, '_blank'); });
+    }
   }
 }
