f592dab34148f752cad24a43bdb652deb25c61c2
Edward Bala Yansen <edward.by@ad-ins.com>
2020-07-10 13:46:17 +0700

    change url

--- src/app/shared/constant/URLConstant.ts
+++ src/app/shared/constant/URLConstant.ts
@@ -1,10 +1,6 @@
 import { environment } from "environments/environment";
 
 export class URLConstant {
-  public static GetRefMasterByMasterCode = environment.FoundationR3Url + "/RefMaster/GetRefMasterByMasterCode";
-  public static GetRefMasterByRefMasterTypeCode = environment.FoundationR3Url + "/RefMaster/GetRefMasterByRefMasterTypeCode"
-  public static GetListRefMasterByRefMasterTypeCodes = environment.FoundationR3Url + "/RefMaster/GetListRefMasterByRefMasterTypeCodes";
-
     // NOTIFICATION HUB
     public static WebSocketUrl = environment.WebSocketURL + "/Notificationhub";
     public static GetListNotificationHByRefUserId = environment.FoundationR3Url + "/NotificationH/GetListNotificationHByRefUserId";
