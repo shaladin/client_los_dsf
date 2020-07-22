1d0b1aa9c0f1deddcae11ef829992e1337cbca29
Jason Andreas Widjaja <jason.aw@ad-ins.com>
2020-07-22 11:50:08 +0700

    cfna

--- src/app/NEW-NAP/CFNA/input-nap-routing.module.ts
+++ src/app/NEW-NAP/CFNA/input-nap-routing.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FinancialDataComponent } from '../sharing-component/input-nap-component/financial-data/financial-data.component';
+import { NapPagingComponent } from './nap-paging/nap-paging.component';
+import { NapAddComponent } from './nap-add/nap-add.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      // {
-      //     path: 'Paging',
-      //     component: "",
-      //     data: {
-      //         title: 'Paging'
-      //     }
-      // },
-      // {
-      //     path: 'Add',
-      //     component: "",
-      //     data: {
-      //         title: 'Add'
-      //     }
-      // },
+      {
+          path: 'Paging',
+          component: NapPagingComponent,
+          data: {
+              title: 'Paging'
+          }
+      },
+      {
+        path: 'Add',
+        component: NapAddComponent,
+        data: {
+          title: 'New Application Add'
+        }
+      },
       // {
       //     path: 'Add/Detail',
       //     component: "",
--- src/app/NEW-NAP/CFNA/input-nap.module.ts
+++ src/app/NEW-NAP/CFNA/input-nap.module.ts
@@ -5,6 +5,8 @@ import { AdInsModule } from "app/components/adins-module/adins.module";
 import { InputNapCFNARoutingModule } from "./input-nap-routing.module";
 import { ArchwizardModule } from "angular-archwizard";
 import { InputNapComponentModule } from "app/NEW-NAP/sharing-component/input-nap-component/input-nap-component.module";
+import { NapPagingComponent } from './nap-paging/nap-paging.component';
+import { NapAddComponent } from './nap-add/nap-add.component';
 
 @NgModule({
   imports: [
@@ -18,7 +20,8 @@ import { InputNapComponentModule } from "app/NEW-NAP/sharing-component/input-nap
     // NapAddComponent,
     // NapPagingComponent,
     // NapAddDetailComponent
-  ],
+  NapPagingComponent,
+    NapAddComponent],
   providers: [
     NGXToastrService
   ]
