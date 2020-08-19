b21dccbf1daf2a708e25b4b23457134ac6f6e957
Steven Lianto Halim <steven.lh@ad-ins.com>
2020-08-19 16:40:28 +0700

    POC DMS

---
+++ src/app/shared/pipe/safepipe.ts
@@ -0,0 +1,13 @@
+import { PipeTransform, Pipe, SecurityContext } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+
+@Pipe({ name : 'safe'})
+export class SafePipe implements PipeTransform{
+    constructor(private sanitizer: DomSanitizer) {
+        console.log("SANITIZER");
+    }
+    transform(url : string) {
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    }
+
+}
