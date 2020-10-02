d1152f2ed88dd5d31e8c5c489e7b78fee394a281
Vincent Andreas <vincent.andreas@ad-ins.com>
2020-10-02 15:32:06 +0700

    app-single-coll-data

--- src/app/NEW-NAP/sharing-component/input-nap-component/application-data/application-data.component.ts
+++ src/app/NEW-NAP/sharing-component/input-nap-component/application-data/application-data.component.ts
@@ -298,7 +298,7 @@ export class ApplicationDataComponent implements OnInit {
         
         this.makeNewLookupCriteria();
         this.getInterestTypeCode();
-        this.getDDLFromProdOffering(CommonConstant.RefMasterTypeCodeInstSchm);
+        // this.getDDLFromProdOffering(CommonConstant.RefMasterTypeCodeInstSchm);
         this.getDDLFromProdOffering(CommonConstant.RefMasterTypeCodePayFreq);
         this.getDDLFromProdOffering(CommonConstant.RefProdCompFirstInstType);
         this.getPayFregData();
