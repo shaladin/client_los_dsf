cf7c3c72e3e414a2dbba929f5588c7d7a28d33d7
Xania Yoan Apsari <xania.ya@ad-ins.com>
2020-10-27 13:13:12 +0700

    ganti json

--- src/app/NEW-NAP/CF4W/nap-cust-main-data/nap-cust-main-data.component.ts
+++ src/app/NEW-NAP/CF4W/nap-cust-main-data/nap-cust-main-data.component.ts
@@ -67,7 +67,7 @@ export class NapCustMainDataComponent implements OnInit {
   ngOnInit() {
     this.ClaimTask();
     this.AppStepIndex = 0;
-    this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewNapAppMainData.json";
+    this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewNapAppMainInformation.json";
     this.viewGenericObj.viewEnvironment = environment.losUrl;
     this.viewGenericObj.ddlEnvironments = [
       {
