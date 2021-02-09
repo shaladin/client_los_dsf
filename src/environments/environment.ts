5bc8030300493a87f7426623d9c644ee9c6ca2a6
vincent.andreas <vincent.andreas89@gmail.com>
2021-02-09 12:00:00 +0700

    ENV SEMENTARA

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,14 +8,14 @@ export const environment = {
   navbarColor: 'transparent',
   // losUrl: 'http://localhost:5001',
   // FoundationR3Url: 'https://localhost:5000',
-  losUrl: 'http://r3app-server.ad-ins.com/LOS',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
+  losUrl: 'http://r3app-server.ad-ins.com/LOS_SIT_CY4',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
   DMSUrl : "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
   ApprovalR3Url: 'http://r3app-server.ad-ins.com/approval',
   FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
   losR3Web: 'http://r3web-server/LOSR3',
-  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
-  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
+  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_SIT_CY4',
+  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
   Module:"LOS"
 };
