52e8b477f507ed2934f24a3b18f4cbec881de704
vincent.andreas <vincent.andreas89@gmail.com>
2021-02-15 13:19:22 +0700

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,16 +6,16 @@
 export const environment = {
   production: false,
   navbarColor: 'transparent',
-  // losUrl: 'http://localhost:5001',
+  losUrl: 'http://localhost:5001',
   // FoundationR3Url: 'https://localhost:5000',
-  losUrl: 'http://r3app-server.ad-ins.com/LOS_SIT_CY4',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
+  // losUrl: 'http://r3app-server.ad-ins.com/LOS_R3',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   DMSUrl : "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
   ApprovalR3Url: 'http://r3app-server.ad-ins.com/approval',
   FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
   losR3Web: 'http://r3web-server/LOSR3',
-  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_SIT_CY4',
-  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
+  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
+  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
   Module:"LOS"
 };
