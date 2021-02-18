0d5574d6ccac40bba6117a4e975cb77f89e63382
Reynard <reynard.wf@ad-ins.com>
2021-02-18 13:22:32 +0700

    typo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ import { NavigationConstant } from './shared/constant/NavigationConstant';
 const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: "/Pages/Login",
+    redirectTo: "/pages/login",
     pathMatch: 'full',
   },
   { path: '', component: FullLayoutComponent, data: { title: 'full Views' }, children: Full_ROUTES, canActivate: [AuthGuard] },
