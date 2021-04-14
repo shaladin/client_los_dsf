7dcd573015b253776bd00813fd36aa057e53ce8f
Gerry Gelvianlo <gerry.gelvianlo@ad-ins.com>
2021-04-14 15:16:55 +0700

    Membuat Copy Cancel untuk OPL

--- src/app/NEW-NAP/business-process/additional-process/copy-cancelled-application/copy-cancelled-application.component.ts
+++ src/app/NEW-NAP/business-process/additional-process/copy-cancelled-application/copy-cancelled-application.component.ts
@@ -5,10 +5,11 @@ import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { UcpagingComponent } from '@adins/ucpaging';
 import { HttpClient } from '@angular/common/http';
 import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { URLConstant } from 'app/shared/constant/URLConstant';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 import { AdInsHelper } from 'app/shared/AdInsHelper';
+import { CommonConstant } from 'app/shared/constant/CommonConstant';
 
 @Component({
   selector: 'app-copy-cancelled-application',
@@ -21,7 +22,7 @@ export class CopyCancelledApplicationComponent implements OnInit {
   BizTemplateCode: string;
   IsNapVersionMainData: boolean = false;
 
-  constructor(private http: HttpClient, private toastr: NGXToastrService, private router: Router,
+  constructor(private http: HttpClient, private toastr: NGXToastrService,
     private route: ActivatedRoute) { 
     this.route.queryParams.subscribe(params => {
       if (params["BizTemplateCode"] != null) {
@@ -40,25 +41,54 @@ export class CopyCancelledApplicationComponent implements OnInit {
     critObj.propName = 'a.BIZ_TEMPLATE_CODE';
     critObj.value = this.BizTemplateCode;
     
-    this.inputPagingObj._url = "./assets/ucpaging/searchCancelledApp.json";
+    if(this.BizTemplateCode === CommonConstant.OPL) {
+      this.inputPagingObj._url = "./assets/ucpaging/new-nap/business-process/additional-process/copy-cancelled-application/search-cancelled-app-opl.json";
+      this.inputPagingObj.pagingJson = "./assets/ucpaging/new-nap/business-process/additional-process/copy-cancelled-application/search-cancelled-app-opl.json";
+      this.inputPagingObj.ddlEnvironments = [
+        {
+          name: "A.ORI_OFFICE_CODE",
+          environment: environment.FoundationR3Url
+        }
+      ];
+    }
+    else {
+      this.inputPagingObj._url = "./assets/ucpaging/searchCancelledApp.json";
+      this.inputPagingObj.pagingJson = "./assets/ucpaging/searchCancelledApp.json";
+    }
     this.inputPagingObj.enviromentUrl = environment.losUrl;
     this.inputPagingObj.apiQryPaging = URLConstant.GetPagingObjectBySQL;
-    this.inputPagingObj.pagingJson = "./assets/ucpaging/searchCancelledApp.json";
 
     this.inputPagingObj.addCritInput.push(critObj);
   }
 
   getEvent(ev) {
-    if(ev.Key == "prodOff"){
+    if(ev.Key == "prodOff") {
       AdInsHelper.OpenProdOfferingViewByCodeAndVersion(ev.RowObj.ProdOfferingCode, ev.RowObj.ProdOfferingVersion);
-    }else if(ev.Key == "copy"){
+    }
+    else if(ev.Key == "copy") {
       if (confirm("Are you sure to copy this application?")) {
-        var url = this.IsNapVersionMainData ? URLConstant.CopyCancelledAppForMainData : URLConstant.CopyCancelledApp;
-        this.http.post(url, { AppId: ev.RowObj.AppId }).subscribe(
-          response => {
-            this.toastr.successMessage(response["message"]);
-            this.paging.searchPagination(1);
-          });
+        if(this.BizTemplateCode === CommonConstant.OPL) {
+          var obj = {
+            AppNo: ev.RowObj.AppNo,
+            OriOfficeCode: ev.RowObj.OfficeCode,
+            IsCopyCancel: true
+          };
+          this.http.post(URLConstant.AddNewApplicationOplFromCopy, obj).subscribe(
+            response => {
+              this.toastr.successMessage(response["message"]);
+              this.paging.searchPagination(1);
+            }
+          );
+        }
+        else {
+          var url = this.IsNapVersionMainData ? URLConstant.CopyCancelledAppForMainData : URLConstant.CopyCancelledApp;
+          this.http.post(url, { AppId: ev.RowObj.AppId }).subscribe(
+            response => {
+              this.toastr.successMessage(response["message"]);
+              this.paging.searchPagination(1);
+            }
+          );
+        }
       }
     }
   }
