ad796cdb63743b67c532e6da6dd1def9b276cbe4
Xania Yoan <xania.ya@ad-ins.com>
2021-04-27 09:47:26 +0700

    removed tempURL

--- src/app/NEW-NAP/sharing-page/nap-from-lead/detail/nap-from-lead-detail.component.ts
+++ src/app/NEW-NAP/sharing-page/nap-from-lead/detail/nap-from-lead-detail.component.ts
@@ -117,7 +117,7 @@ export class NapFromLeadDetailComponent implements OnInit {
   MakeLookUpObj() {
     this.inputLookupObjName = new InputLookupObj();
     this.inputLookupObjName.urlJson = "./assets/uclookup/NAP/lookupAppName.json";
-    this.inputLookupObjName.urlEnviPaging = environment.tempUrl;
+    this.inputLookupObjName.urlEnviPaging = environment.losUrl;
     this.inputLookupObjName.pagingJson = "./assets/uclookup/NAP/lookupAppName.json";
     this.inputLookupObjName.genericJson = "./assets/uclookup/NAP/lookupAppName.json";
     this.inputLookupObjName.nameSelect = this.NapAppForm.controls.ProdOfferingName.value;
