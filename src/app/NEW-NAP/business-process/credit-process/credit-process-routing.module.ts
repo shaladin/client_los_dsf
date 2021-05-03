ea5b18d53b67b83d3c0041b36a0b2733f525882a
Xania Yoan <xania.ya@ad-ins.com>
2021-05-03 10:56:08 +0700

    PBI-4527 : Removed unused component

--- src/app/NEW-NAP/business-process/credit-process/credit-process-routing.module.ts
+++ src/app/NEW-NAP/business-process/credit-process/credit-process-routing.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PhoneVerificationPagingComponent } from './phone-verification/phone-verification-paging/phone-verification-paging.component';
-import { CreditApprovalPagingComponent } from './credit-approval/credit-approval-paging/credit-approval-paging.component';
-import { CreditInvestigationPagingComponent } from './credit-investigation/credit-investigation-paging/credit-investigation-paging.component';
-import { CreditInvestigationDetailComponent } from './credit-investigation/credit-investigation-detail/credit-investigation-detail.component';
-import { CreditReviewPagingComponent } from './credit-review/credit-review-paging/credit-review-paging.component';
-import { CreditReviewMainComponent } from './credit-review/credit-review-main/credit-review-main.component';
 import { CommissionReservedFundDetailComponent } from './commission-reserved-fund/comission-reserved-fund-detail/commission-reserved-fund-detail.component';
 import { CommissionReservedFundPagingComponent } from './commission-reserved-fund/comission-reserved-fund-paging/commission-reserved-fund-paging.component';
 import { PhoneVerificationSubjectComponent } from './phone-verification/phone-verification-subject/phone-verification-subject.component';
 import { PhoneVerificationSubjectVerifComponent } from './phone-verification/phone-verification-subject-verif/phone-verification-subject-verif.component';
 import { PhoneVerificationSubjectViewComponent } from './phone-verification/phone-verification-subject-view/phone-verification-subject-view.component';
 import { CreditInquiryComponent } from './credit-inquiry/credit-inquiry.component';
-import { CreditApprovalDetailComponent } from './credit-approval/credit-approval-detail/credit-approval-detail.component';
-import { NewCreditInvestigationDetailComponent } from './new-credit-investigation/new-credit-investigation-detail/new-credit-investigation-detail.component';
-import { NewCreditInvestigationComponent } from './new-credit-investigation/new-credit-investigation.component';
-import { CreditReviewCfnaComponent } from './credit-review-cfna/credit-review-cfna-detail/credit-review-cfna.component';
-import { CreditReviewCfnaPagingComponent } from './credit-review-cfna/credit-review-cfna-paging/credit-review-cfna-paging.component';
-import { CreditApprovalCfnaComponent } from './credit-approval-cfna/credit-approval-cfna-detail/credit-approval-cfna.component';
-import { CreditApprovalCfnaPagingComponent } from './credit-approval-cfna/credit-approval-cfna-paging/credit-approval-cfna-paging.component';
 import { CreditReviewComponent } from './credit-review-prototype/credit-review-paging/credit-review-paging.component';
 import { CreditReviewDetailPersonalComponent } from './credit-review-prototype/credit-review-detail-personal/credit-review-detail-personal.component';
 import { CreditReviewCrDetailComponent } from './credit-review-cr/credit-review-cr-detail/credit-review-cr-detail.component';
@@ -75,41 +63,6 @@ const routes: Routes = [
         }
       },
       {
-        path: PathConstant.CRD_INVESTIGATION_PAGING,
-        component: CreditInvestigationPagingComponent,
-        data: {
-          title: 'Credit Investigation'
-        }
-      },
-      {
-        path: PathConstant.CRD_INVESTIGATION_DETAIL,
-        component: CreditInvestigationDetailComponent,
-        data: {
-          title: 'Credit Investigation'
-        }
-      },
-      {
-        path: PathConstant.NEW_CRD_INVESTIGATION_PAGING,
-        component: NewCreditInvestigationComponent,
-        data: {
-          title: 'Credit Investigation'
-        }
-      },
-      {
-        path: PathConstant.NEW_CRD_INVESTIGATION_DETAIL,
-        component: NewCreditInvestigationDetailComponent,
-        data: {
-          title: 'Credit Investigation'
-        }
-      },
-      {
-        path: PathConstant.CRD_REVIEW_PAGING,
-        component: CreditReviewPagingComponent,
-        data: {
-          title: 'Credit Review Paging'
-        }
-      },
-      {
         path: PathConstant.CRD_REVIEW_PROTOTYPE_PAGING,
         component: CreditReviewComponent,
         data: {
@@ -121,55 +74,6 @@ const routes: Routes = [
         component: CreditReviewDetailPersonalComponent,
         data: {
           title: 'Credit Review Detail'
-        }
-      },
-      {
-        path: PathConstant.CRD_REVIEW_MAIN,
-        component: CreditReviewMainComponent,
-        data: {
-          title: 'Credit Review Main'
-        }
-      },
-      {
-        path: PathConstant.CRD_REVIEW_CFNA_PAGING,
-        component: CreditReviewCfnaPagingComponent,
-        data: {
-          title: 'Credit Review Paging'
-        }
-      },
-      {
-        path: PathConstant.CRD_REVIEW_CFNA_MAIN,
-        component: CreditReviewCfnaComponent,
-        data: {
-          title: 'Credit Review Main'
-        }
-      },
-      {
-        path: PathConstant.CRD_APPRV_PAGING,
-        component: CreditApprovalPagingComponent,
-        data: {
-          title: 'Credit Approval Paging'
-        }
-      },
-      {
-        path: PathConstant.CRD_APPRV_DETAIL,
-        component: CreditApprovalDetailComponent,
-        data: {
-          title: 'Credit Approval Detail'
-        }
-      },
-      {
-        path: PathConstant.CRD_APPRV_CFNA_PAGING,
-        component: CreditApprovalCfnaPagingComponent,
-        data: {
-          title: 'Credit Approval Paging'
-        }
-      },
-      {
-        path: PathConstant.CRD_APPRV_CFNA_DETAIL,
-        component: CreditApprovalCfnaComponent,
-        data: {
-          title: 'Credit Approval Detail'
         }
       },
       {
