07bb9ed711f20f99c9c8da13b53805a46d5fd761
ivan wijaya <ivan.wijaya@ad-ins.com>
2021-05-28 12:01:14 +0700

    merge NEW-NAP/DLFN dari DSF

---
+++ src/app/NEW-NAP/DLFN/nap-paging/nap-paging.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { UcPagingObj } from 'app/shared/model/UcPagingObj.Model';
+import { environment } from 'environments/environment';
+import { AdInsConstant } from 'app/shared/AdInstConstant';
+import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
+import { HttpClient } from '@angular/common/http';
+import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CenterGrpOfficeMbrObj } from 'app/shared/model/RefOffice/CenterGrpOfficeMbrObj.Model';
+import { AdInsHelper } from 'app/shared/AdInsHelper';
+import { CommonConstant } from 'app/shared/constant/CommonConstant';
+import { URLConstant } from 'app/shared/constant/URLConstant';
+import { CookieService } from 'ngx-cookie';
+import { CurrentUserContext } from 'app/shared/model/CurrentUserContext.model';
+
+@Component({
+  selector: 'app-nap-paging',
+  templateUrl: './nap-paging.component.html'
+})
+export class NapPagingComponent implements OnInit {
+  inputPagingObj: UcPagingObj;
+  arrCrit: Array<CriteriaObj>;
+  userAccess: CurrentUserContext;
+
+  constructor(
+    private http: HttpClient,
+    private toastr: NGXToastrService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private cookieService: CookieService
+  ) {
+    this.route.queryParams.subscribe(params => {
+      if (params['BizTemplateCode'] != null) {
+        localStorage.setItem("BizTemplateCode", params['BizTemplateCode']);
+      }
+    });
+  }
+
+  ngOnInit() {
+    this.userAccess = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+
+    this.arrCrit = new Array();
+    this.makeCriteria();
+
+    this.inputPagingObj = new UcPagingObj();
+    this.inputPagingObj._url = "./assets/ucpaging/searchApp.json";
+    this.inputPagingObj.enviromentUrl = environment.losUrl;
+    this.inputPagingObj.apiQryPaging = URLConstant.GetPagingObjectBySQL;
+    this.inputPagingObj.pagingJson = "./assets/ucpaging/searchApp.json";
+
+    this.inputPagingObj.ddlEnvironments = [
+      {
+        name: "a.ORI_OFFICE_CODE",
+        environment: environment.FoundationR3Url
+      }
+    ];
+
+    this.inputPagingObj.addCritInput = this.arrCrit;
+  }
+
+  makeCriteria() {
+    let critObj = new CriteriaObj();
+    critObj.restriction = AdInsConstant.RestrictionLike;
+    critObj.propName = 'WTL.ACT_CODE';
+    critObj.value = "NAP_" + CommonConstant.DF;
+    this.arrCrit.push(critObj);
+
+    critObj = new CriteriaObj();
+    critObj.restriction = AdInsConstant.RestrictionIn;
+    if (this.userAccess.MrOfficeTypeCode != CommonConstant.CENTER_GROUP_CODE) {
+      critObj.propName = 'a.ORI_OFFICE_CODE';
+      critObj.listValue = [this.userAccess.OfficeCode];
+    } else {
+      critObj.propName = 'a.ORI_OFFICE_CODE';
+      let obj = { Code: CommonConstant.CENTER_GROUP_CODE };
+      this.http.post(URLConstant.GetListCenterGrpMemberByCenterGrpCode, obj).subscribe(
+        (response) => {
+          let CenterGrpOfficeMbrObjs: Array<CenterGrpOfficeMbrObj> = response["ListCenterGrpOfficeMbr"];
+
+          let listDataTemp = new Array();
+          for (var i = 0; i < CenterGrpOfficeMbrObjs.length; i++) {
+            listDataTemp.push(CenterGrpOfficeMbrObjs[i].RefOfficeCode);
+          }
+          critObj.listValue = listDataTemp;
+        })
+    }
+    // critObj.value = localStorage.getItem("LobCode");
+    this.arrCrit.push(critObj);
+  }
+
+  AddApp() {
+    let obj = { Code: this.userAccess.OfficeCode };
+    this.http.post(URLConstant.GetRefOfficeByOfficeCode, obj).subscribe(
+      (response) => {
+        if (response["IsAllowAppCreated"] == true) {
+          AdInsHelper.RedirectUrl(this.router, ["Nap/DLFN/Add"], {});
+        } else {
+          this.toastr.typeErrorCustom('Office Is Not Allowed to Create App');
+        }
+      });
+  }
+
+  GetCallBack(ev: any) {
+    if (ev.Key == "ViewProdOffering") {
+      AdInsHelper.OpenProdOfferingViewByCodeAndVersion(ev.RowObj.prodOfferingCode, ev.RowObj.prodOfferingVersion);
+    }
+    if (ev.Key == "Edit") {
+      AdInsHelper.RedirectUrl(this.router, ["Nap/DLFN/Add/Detail"], { "AppId": ev.RowObj.AppId, "WfTaskListId": ev.RowObj.WfTaskListId });
+    }
+  }
+}
