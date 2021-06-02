b6294837ed840281238379c696d5443a720a2382
Timothy Gracia Yaputra <timothy.gy@ad-ins.com>
2021-06-02 17:41:18 +0700

    Penyesuaian Build&Environment SIT untuk DSF Merge

--- src/environments/environment.sit.ts
+++ src/environments/environment.sit.ts
@@ -8,19 +8,19 @@ export const environment = {
   navbarColor: 'transparent',
   //losUrl : 'http://localhost:5001',
   // FoundationR3Url: 'http://localhost:5000',
-  losUrl: 'http://r3app-server.ad-ins.com/LOS_SIT_CY4',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
+  losUrl: 'http://r3impl-appsvr.ad-ins.com/LOSDSFSITMERGE',
+  FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUDSFSITMERGE',
   DMSUrl : "http://sky.ad-ins.com/LiteDMS/Integration/ViewDoc.aspx",
   AMSUrl : "http://r3app-server.ad-ins.com/AMS", //OPL dah ada
   LMSUrl : "http://r3app-server.ad-ins.com/LMS", //OPL dah ada
-  ApprovalR3Url: 'http://r3app-server.ad-ins.com/APPROVAL_SIT',
-  FoundationR3Web: 'http://r3web-server.ad-ins.com/FOUNDATION_SIT_CY4',
-  losR3Web: 'http://r3web-server.ad-ins.com/LOS_SIT_CY4',
-  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_SIT_CY4',
-  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_SIT_CY4',
+  ApprovalR3Url: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_DSF_SIT',
+  FoundationR3Web: 'http://r3impl-websvr.ad-ins.com/FOUDSF_SIT',
+  losR3Web: 'http://r3impl-websvr.ad-ins.com/LOSDSF_SIT',
+  WorkflowR3Url: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_DSF_SIT',
+  WebSocketURL: 'http://r3impl-appsvr.ad-ins.com/FOUDSFSITMERGE',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
   dmsURL: 'http://kfx-svr/LITEDMS_OPL/LiteDMS/pageconfins.aspx',
-  WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
+  WFThingsToDoUrl: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_DSF_SIT/',
   ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
   ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
   Module: "LOS"
