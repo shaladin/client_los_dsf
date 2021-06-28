41c62837ff908205f94b703914f5e991a1656dc4
richard suprayogi <richard.suprayogi@ad-ins.com>
2021-06-28 13:56:30 +0700

    fixing link suppl,link cust mode normal+return

--- src/app/MOU/mou-customer-request/mou-detail-factoring/mou-detail-factoring.component.ts
+++ src/app/MOU/mou-customer-request/mou-detail-factoring/mou-detail-factoring.component.ts
@@ -26,10 +26,9 @@ import { AdInsConstant } from 'app/shared/AdInstConstant';
 export class MouDetailFactoringComponent implements OnInit {
   @Input() MouCustId: number;
   @Output() ResponseMouCustFactoring: EventEmitter<any> = new EventEmitter();
-  @ViewChild(MouCustListedCustFctrComponent) 
   user: CurrentUserContext = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
   isWithoutRecourse: boolean
-  MouListedFctrComp: MouCustListedCustFctrComponent;
+  @ViewChild(MouCustListedCustFctrComponent) MouListedFctrComp: MouCustListedCustFctrComponent;
   recourseTypeList: Array<KeyValueObj>;
   wopList: Array<KeyValueObj>;
   paidByList: Array<KeyValueObj>;
@@ -140,18 +139,55 @@ export class MouDetailFactoringComponent implements OnInit {
         if(mouFctrData["MouCustFctrId"] != 0){
           this.mode = "edit";
           this.MouDetailFactoringForm.patchValue({
-            ...mouFctrData
+            // ...mouFctrData
+            MouCustFctrId: mouFctrData["MouCustFctrId"],
+            MouCustId: mouFctrData["MouCustId"],
+            MrRecourseTypeCode: mouFctrData["MrRecourseTypeCode"],
+            IsDisclosed: mouFctrData["IsDisclosed"],
+            WopCode: mouFctrData["WopCode"],
+            MrPaidByCode: mouFctrData["MrPaidByCode"],
+            MrInstTypeCode: mouFctrData["MrInstTypeCode"],
+            SingleInstCalcMthd: mouFctrData["SingleInstCalcMthd"],
+            TopDays: mouFctrData["TopDays"],
+            TenorFrom: mouFctrData["TenorFrom"],
+            TenorTo: mouFctrData["TenorTo"],
+            PayFreqCode: mouFctrData["PayFreqCode"],
+            MrInstSchmCode: mouFctrData["MrInstSchmCode"],
+            InterestRatePrcnt: mouFctrData["InterestRatePrcnt"],
+            RetentionPrcnt: mouFctrData["RetentionPrcnt"],
+            IsListedCust: mouFctrData["IsListedCust"],
+            Notes: mouFctrData["Notes"],
+            CurrCode: mouFctrData["CurrCode"],
+            RowVersion: mouFctrData["RowVersion"],
+            VendorCode: mouFctrData["VendorCode"],
+            VendorName: mouFctrData["VendorName"],
+            VendorId: mouFctrData["VendorId"],
+            CustNo: mouFctrData["CustNo"],
+            CustName: mouFctrData["CustName"],
+            MrCustTypeCode: mouFctrData["MrCustTypeCode"],
+            VirtualAccNo: mouFctrData["VirtualAccNo"]
           });
+
+          var objVendor = {
+            Code: mouFctrData["VendorCode"]
+          }
+          this.httpClient.post(URLConstant.GetVendorByVendorCode, objVendor).subscribe(
+            (responseVendor) => {
+              this.inputLookupObj.nameSelect = responseVendor["VendorName"];
+              this.inputLookupObj.jsonSelect = { VendorName: responseVendor["VendorName"] , VendorCode: responseVendor["VendorCode"] };
+            }
+          )
         }
         else{
           this.MouDetailFactoringForm.patchValue({
             MouCustId: this.MouCustId
           });
         }
-        this.OnChangeRecourseType(this.recourseTypeList[0].Key);
+        this.isWithoutRecourse = false
         this.CheckPaidBy(this.MouDetailFactoringForm.controls.MrPaidByCode.value);
         this.instTypeHandler();
         this.shouldComponentLoad = true;
+        this.OnChangeRecourseType(this.MouDetailFactoringForm.controls.MrRecourseTypeCode.value)
       });
   }
 
@@ -249,6 +285,11 @@ export class MouDetailFactoringComponent implements OnInit {
     if(value == CommonConstant.PAID_BY_CUST_FCTR){
       this.MouDetailFactoringForm.controls.IsDisclosed.disable();
       this.MouDetailFactoringForm.controls.IsDisclosed.setValue(true);
+      this.MouDetailFactoringForm.patchValue({
+        MrRecourseTypeCode: CommonConstant.WITHOUT_RECOURSE_TYPE
+      });
+      this.OnChangeRecourseType(CommonConstant.WITHOUT_RECOURSE_TYPE);
+      
     }else if(value == CommonConstant.PAID_BY_CUST){
       this.MouDetailFactoringForm.controls.IsDisclosed.enable();
       this.MouDetailFactoringForm.controls.IsDisclosed.setValue(false);
@@ -290,17 +331,17 @@ export class MouDetailFactoringComponent implements OnInit {
       ];
       this.inputLookupCustObj.isReady = true;
 
-      this.httpClient.post<Array<MouCustListedCustFctrObj>>(URLConstant.GetListMouCustListedCustFctrByMouCustId, { MouCustId: this.MouCustId }).subscribe(
+      this.httpClient.post(URLConstant.GetListMouCustListedCustFctrByMouCustId, { Id: this.MouCustId }).subscribe(
         (response) => {
-          this.listedCusts = response;
+          this.listedCusts = response[CommonConstant.ReturnObj];
 
           if (this.listedCusts.length > 0) {
             this.MouDetailFactoringForm.patchValue({
               CustNo: this.listedCusts[0].CustNo
             });
 
-            this.inputLookupCustObj.nameSelect = this.listedCusts[0].CustNo
-            this.inputLookupCustObj.jsonSelect = { CustNo: this.listedCusts[0].CustName };
+            this.inputLookupCustObj.nameSelect = this.listedCusts[0].CustName;
+            this.inputLookupCustObj.jsonSelect = { CustNo: this.listedCusts[0].CustNo , CustName: this.listedCusts[0].CustName };
             this.setCustName(this.listedCusts[0].CustNo, this.listedCusts[0].MouListedCustFctrId)
           }
         })
