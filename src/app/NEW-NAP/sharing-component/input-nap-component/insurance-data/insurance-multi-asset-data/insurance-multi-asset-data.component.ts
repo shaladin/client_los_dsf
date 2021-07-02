150ca11d88b8572ee60eb419d573f9b09e8454c8
Edward Bala Yansen <edward.by@ad-ins.com>
2021-07-02 14:48:58 +0700

    custInsco len if CUCO

--- src/app/NEW-NAP/sharing-component/input-nap-component/insurance-data/insurance-multi-asset-data/insurance-multi-asset-data.component.ts
+++ src/app/NEW-NAP/sharing-component/input-nap-component/insurance-data/insurance-multi-asset-data/insurance-multi-asset-data.component.ts
@@ -1457,6 +1457,7 @@ export class InsuranceMultiAssetDataComponent implements OnInit {
     return new Date(date.getTime() - userTimezoneOffset);
   }
 
+  insLenCust: number = 0;
   GetGetCuCoInsLength() {
     let tempForm = this.InsuranceDataForm as FormGroup;
     let tempReq: InsuranceLenObj = new InsuranceLenObj();
@@ -1474,16 +1475,18 @@ export class InsuranceMultiAssetDataComponent implements OnInit {
         if (tempReq.MrCoverPeriod != CommonConstant.CoverPeriodOverTenor) {
           this.minInsLength = 1;
           this.maxInsLength = 9999;
-          tempInsLength.patchValue(InsuranceLen);
           tempInsLength.enable();
-          if (tempReq.MrCoverPeriod == CommonConstant.CoverPeriodFullTenor || tempReq.MrCoverPeriod == CommonConstant.CoverPeriodAnnually) tempInsLength.disable();
+          if (tempReq.MrCoverPeriod == CommonConstant.CoverPeriodFullTenor || tempReq.MrCoverPeriod == CommonConstant.CoverPeriodAnnually){
+            tempInsLength.disable();
+            tempInsLength.patchValue(InsuranceLen);
+          } 
           if (tempReq.MrCoverPeriod == CommonConstant.CoverPeriodPartialTenor) this.maxInsLength = InsuranceLen;
         } else {
           this.minInsLength = InsuranceLen + 1;
           this.maxInsLength = 9999;
-          tempInsLength.patchValue(InsuranceLen + 1);
           tempInsLength.enable();
         }
+        this.insLenCust = this.appObj.Tenor - InsuranceLen;
         tempInsLength.setValidators([Validators.required, Validators.min(this.minInsLength), Validators.max(this.maxInsLength)]);
         tempInsLength.updateValueAndValidity();
       }
