4f0a5e187d09cb621494d9c65789ea7e3d86b020
Timothy Gracia Yaputra <timothy.gy@ad-ins.com>
2021-07-03 10:22:23 +0700

    SITCFRTHREE-1519 - Perbaikan Main Info View NAP 1 DF

--- src/app/NEW-NAP/DLFN/nap-cust-main-data/nap-cust-main-data.component.ts
+++ src/app/NEW-NAP/DLFN/nap-cust-main-data/nap-cust-main-data.component.ts
@@ -81,7 +81,7 @@ export class NapCustMainDataComponent implements OnInit {
   ngOnInit() {
     this.ClaimTask();
     this.AppStepIndex = 0;
-    this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewNapAppMainInformation.json";
+    this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewNapAppMainInformationDF.json";
     this.viewGenericObj.viewEnvironment = environment.losUrl;
     this.viewGenericObj.ddlEnvironments = [
       {
