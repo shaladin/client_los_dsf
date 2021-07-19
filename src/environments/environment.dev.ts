422a82d6f8f67b9fe1b121209e47b888a038c6df
Timothy Gracia Yaputra <timothy.gy@ad-ins.com>
2021-07-19 15:02:11 +0700

    Perbaikan Setting Envi untuk LOS DSF IMPL

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -8,17 +8,17 @@ export const environment = {
   navbarColor: 'transparent',
   //losUrl: 'http://localhost:5001',
   // FoundationR3Url: 'http://localhost:5000',
-  losUrl: 'http://r3app-server.ad-ins.com/LOS_DEV',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
-  DMSUrl : "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
-  AMSUrl : "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
-  LMSUrl : "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
-  ApprovalR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV/Approval',
+  losUrl: 'http://r3impl-appsvr.ad-ins.com/LOS_DSF_IMPL',
+  FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUNDATION_DSF_IMPL',
+  DMSUrl: "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
+  AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
+  LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
+  ApprovalR3Url: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_R3_BE_DSF_IMPL',
   ApprovalR3OplUrl: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_OPL_BE_R3/',
-  FoundationR3Web: 'http://r3web-server.ad-ins.com/FOUNDATION_DEV',
-  losR3Web: 'http://r3web-server/LOS_DEV',
-  WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
-  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
+  FoundationR3Web: 'http://r3impl-websvr.ad-ins.com/FOU_DSF_IMPL',
+  losR3Web: 'http://r3impl-websvr.ad-ins.com/LOS_DSF_IMPL',
+  WorkflowR3Url: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_DSF_IMPL',
+  WebSocketURL: 'http://r3impl-appsvr.ad-ins.com/FOU_DSF_IMPL',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
   dmsURL: 'http://kfx-svr/LITEDMS_OPL/LiteDMS/pageconfins.aspx',
   WfR3Url: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
