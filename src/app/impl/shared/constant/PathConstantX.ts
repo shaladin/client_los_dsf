577c0b4a48833d27e88cd99c289b147fd313d0db
Andre Sucipto <andre.sucipto@ad-ins.com>
2021-07-26 11:54:49 +0700

    perbaikan pathconstant

--- src/app/impl/shared/constant/PathConstantX.ts
+++ src/app/impl/shared/constant/PathConstantX.ts
@@ -1,6 +1,9 @@
 import { PathConstant } from 'app/shared/constant/PathConstant';
 export class PathConstantX {
-    public static PGL_DETAIL_X = PathConstant.PGL + "/" + PathConstant.DETAIL + "X";
-    public static DO_MULTI_ASSET_DETAIL_X = PathConstant.DO_MULTI_ASSET + "/" + PathConstant.DETAIL + "X";
-    public static CUST_CONFIRM_DETAIL_X = PathConstant.CUST_CONFIRM + "/" + PathConstant.DETAIL + "X";
+    public static DETAIL_X = PathConstant.DETAIL + "X";
+
+
+    public static PGL_DETAIL_X = PathConstant.PGL + "/" + PathConstantX.DETAIL_X;
+    public static DO_MULTI_ASSET_DETAIL_X = PathConstant.DO_MULTI_ASSET + "/" + PathConstantX.DETAIL_X;
+    public static CUST_CONFIRM_DETAIL_X = PathConstant.CUST_CONFIRM + "/" + PathConstantX.DETAIL_X;
 }
