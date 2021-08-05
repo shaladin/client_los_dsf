8791a14527e435094095fbcd9d5814fcd2b7e114
calvin <calvin.cen@ad-ins.com>
2021-08-05 20:07:46 +0700

    Cekin Fix Support MOU

--- src/app/shared/constant/CommonConstant.ts
+++ src/app/shared/constant/CommonConstant.ts
@@ -918,6 +918,8 @@ export class CommonConstant {
   public static WF_UPL_LEAD = "WF_UPL_LEAD";
   public static UPLOAD_LEAD_REVIEW = "UPLOAD_LEAD_REVIEW";
   public static WF_MOU_GENERAL = "WF_MOU_GENERAL";
+  public static WF_MOU_FACTORING = "WF_MOU_FACTORING";
+  public static WF_MOU_DLFN = "WF_MOU_DLFN";
   public static MOU_REVIEW = "MOU_REVIEW";
   public static LEGAL_RVW = "LEGAL_RVW";
   public static MOU_RETURN = "MOU_RETURN";
