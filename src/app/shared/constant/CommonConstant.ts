e896f27d5e0a938b5561f596daa8240cbc15020f
michael.christian <michael.christian@ad-ins.com>
2021-08-21 13:18:39 +0700

    Backlog-5465: Ubah ApvSchm untuk ChangeMou Review dan MouFreeze

--- src/app/shared/constant/CommonConstant.ts
+++ src/app/shared/constant/CommonConstant.ts
@@ -756,6 +756,14 @@ export class CommonConstant {
   public static SCHM_CODE_AML_APV = "AML_APV_SCHM";
   public static SCHM_CODE_EDIT_APP_AFT_APV_APV_SCHM_NORMAL = "EDIT_APP_AFT_APV_APV_SCHM_NORMAL";
   public static SCHM_CODE_CHG_MOU_APV = "CHG_MOU_APV";
+  public static SCHM_CODE_CHG_MOU_DLFN_APV = "CHG_MOU_DLFN_APV";
+  public static SCHM_CODE_CHG_MOU_FCTR_APV  = "CHG_MOU_FCTR_APV ";
+  public static SCHM_CODE_MOU_FRZ_UNFRZ_DLFN = "MOU_FRZ_UNFRZ_DLFN";
+  public static SCHM_CODE_MOU_FRZ_UNFRZ_FCTR = "MOU_FRZ_UNFRZ_FCTR";
+  public static SCHM_CODE_CHG_MOU_EXP_APV = "CHG_MOU_EXP_APV";
+  public static SCHM_CODE_CHG_MOU_EXP_DLFN_APV = "CHG_MOU_EXP_DLFN_APV";
+  public static SCHM_CODE_CHG_MOU_EXP_FCTR_APV = "CHG_MOU_EXP_FCTR_APV";
+
 
   public static CAT_CODE_MOU_APV_GENERAL = "MOUC_GEN_APV";
   public static CAT_CODE_MOU_APV_DLFN = "MOUC_DLFN_APV";
@@ -842,6 +850,7 @@ export class CommonConstant {
   //MOU TYPE
   public static MOU_TYPE_GENERAL = "GENERAL";
   public static MOU_TYPE_FACTORING = "FACTORING";
+  public static MOU_TYPE_DLFN = "FINANCING";
 
   //Cashflow Item Type
   public static CashFlowItemMasterCodeFeeCptlz = "CASHFLOW_FEE_CPTLZ";
