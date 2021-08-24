18e979f975af5c6f796a6cc2e7ff1b985d830295
Cynthia Purnomo <cynthia.purnomo@ad-ins.com>
2021-08-24 13:32:09 +0700

    BACKLOG-5464 - balikin envi.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@
 export const environment = {
   production: false,
   navbarColor: 'transparent',
-  losUrl: 'https://localhost:5001',
-  FoundationR3Url: 'http://localhost:5000',
-  //losUrl: 'http://r3impl-appsvr.ad-ins.com/LOS_DSF_IMPL',
-  //FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUNDATION_DSF_IMPL',
+  // losUrl: 'https://localhost:5001',
+  // FoundationR3Url: 'http://localhost:5000',
+  losUrl: 'http://r3impl-appsvr.ad-ins.com/LOS_DSF_IMPL',
+  FoundationR3Url: 'http://r3impl-appsvr.ad-ins.com/FOUNDATION_DSF_IMPL',
   DMSUrl: "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
   AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
   LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
