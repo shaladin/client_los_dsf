640571dfce706bde31b49120c77b7a334b2ad06b
ivan wijaya <ivan.wijaya@ad-ins.com>
2021-09-02 17:36:14 +0700

    Backlog-5501 - Penambahan Paging Camunda

--- src/app/impl/NEW-NAP/business-process/additional-process/return-handling/return-handling-survey-verif-x/return-handling-survey-verif-x.component.ts
+++ src/app/impl/NEW-NAP/business-process/additional-process/return-handling/return-handling-survey-verif-x/return-handling-survey-verif-x.component.ts
@@ -6,17 +6,29 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 import { HttpClient } from '@angular/common/http';
 import { URLConstant } from 'app/shared/constant/URLConstant';
+import {AdInsHelper} from 'app/shared/AdInsHelper';
+import {CommonConstant} from 'app/shared/constant/CommonConstant';
+import {CookieService} from 'ngx-cookie';
+import {RequestTaskModelObj} from 'app/shared/model/Workflow/V2/RequestTaskModelObj.model';
+import {IntegrationObj} from 'app/shared/model/library/IntegrationObj.model';
 
 @Component({
   selector: 'app-return-handling-survey-verif-x',
   templateUrl: './return-handling-survey-verif-x.component.html'
 })
 export class ReturnHandlingSurveyVerifXComponent implements OnInit {
+  RequestTaskModel : RequestTaskModelObj = new RequestTaskModelObj();
+  IntegrationObj : IntegrationObj = new IntegrationObj();
 
   BizTemplateCode: string;
-  inputPagingObj: any;
+  inputPagingObj: UcPagingObj;
 
-  constructor(private route: ActivatedRoute,private router: Router,private http: HttpClient) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) {
     this.route.queryParams.subscribe(
       params => {
         if (params["BizTemplateCode"] != null) {
@@ -24,22 +36,46 @@ export class ReturnHandlingSurveyVerifXComponent implements OnInit {
           localStorage.setItem("BizTemplateCode", this.BizTemplateCode);
         }
       }
-    ); 
+    );
   }
 
   ngOnInit() {
     this.inputPagingObj = new UcPagingObj();
-    this.inputPagingObj._url="./assets/ucpaging/survey/searchReturnHandlingSurveyVerif.json";
-    this.inputPagingObj.enviromentUrl = environment.losUrl;
-    this.inputPagingObj.apiQryPaging = "/v1" + URLConstant.GetPagingObjectBySQL;
-    this.inputPagingObj.pagingJson = "./assets/ucpaging/survey/searchReturnHandlingSurveyVerif.json";
-    this.inputPagingObj.ddlEnvironments = [
-      {
-        name: "a.ORI_OFFICE_CODE",
-        environment: environment.FoundationR3Url
-      }
-    ];
-    this.inputPagingObj.addCritInput = this.ActAndOfficeCriteria();
+
+    if(environment.isCore){
+      const UserAccess = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+      this.inputPagingObj._url = "./assets/impl/ucpaging/survey/V2/searchReturnHandlingSurveyVerifV2.json";
+      this.inputPagingObj.pagingJson = "./assets/impl/ucpaging/survey/V2/searchReturnHandlingSurveyVerifV2.json";
+      this.inputPagingObj.isJoinExAPI = true;
+
+      //Request Obj
+      this.RequestTaskModel.ProcessKey = "RTN_ADD_SRVY_" + this.BizTemplateCode;
+      this.RequestTaskModel.TaskDefinitionKey = "ADD_SRVY_" + this.BizTemplateCode;
+      // this.RequestTaskModel.OfficeCode = UserAccess[CommonConstant.OFFICE_CODE];
+      // this.RequestTaskModel.RoleCode = UserAccess[CommonConstant.ROLE_CODE];
+      this.RequestTaskModel.OfficeRoleCodes = [UserAccess[CommonConstant.ROLE_CODE], UserAccess[CommonConstant.OFFICE_CODE]];
+
+      //Integration Obj
+      this.IntegrationObj.baseUrl = URLConstant.GetAllTaskWorkflow;
+      this.IntegrationObj.leftColumnToJoin = "AppNo";
+      this.IntegrationObj.rightColumnToJoin = "ProcessInstanceBusinessKey";
+
+      this.IntegrationObj.requestObj = this.RequestTaskModel;
+      this.inputPagingObj.integrationObj = this.IntegrationObj;
+    }
+    else{
+      this.inputPagingObj._url="./assets/ucpaging/survey/searchReturnHandlingSurveyVerif.json";
+      this.inputPagingObj.enviromentUrl = environment.losUrl;
+      this.inputPagingObj.apiQryPaging = "/v1" + URLConstant.GetPagingObjectBySQL;
+      this.inputPagingObj.pagingJson = "./assets/ucpaging/survey/searchReturnHandlingSurveyVerif.json";
+      this.inputPagingObj.ddlEnvironments = [
+        {
+          name: "a.ORI_OFFICE_CODE",
+          environment: environment.FoundationR3Url
+        }
+      ];
+      this.inputPagingObj.addCritInput = this.ActAndOfficeCriteria();
+    }
   }
 
   ActAndOfficeCriteria() : Array<CriteriaObj> {
--- src/app/impl/NEW-NAP/business-process/credit-process/survey/survey-paging-x/survey-paging-x.component.ts
+++ src/app/impl/NEW-NAP/business-process/credit-process/survey/survey-paging-x/survey-paging-x.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UcpagingModule } from '@adins/ucpaging';
 import { environment } from 'environments/environment';
 import { AdInsConstant } from 'app/shared/AdInstConstant';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,17 +6,29 @@ import { HttpClient } from '@angular/common/http';
 import { CriteriaObj } from 'app/shared/model/CriteriaObj.model';
 import { AdInsHelper } from 'app/shared/AdInsHelper';
 import { URLConstant } from 'app/shared/constant/URLConstant';
+import {UcPagingObj} from 'app/shared/model/UcPagingObj.Model';
+import {CommonConstant} from 'app/shared/constant/CommonConstant';
+import {CookieService} from 'ngx-cookie';
+import {RequestTaskModelObj} from 'app/shared/model/Workflow/V2/RequestTaskModelObj.model';
+import {IntegrationObj} from 'app/shared/model/library/IntegrationObj.model';
 
 @Component({
   selector: 'app-survey-paging-x',
   templateUrl: './survey-paging-x.component.html'
 })
 export class SurveyPagingXComponent implements OnInit {
-  inputPagingObj: any;
-  BizTemplateCode: string;
-  ActCode: string;
+  inputPagingObj: UcPagingObj;
+  RequestTaskModel : RequestTaskModelObj = new RequestTaskModelObj();
+  IntegrationObj : IntegrationObj = new IntegrationObj();
 
-  constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
+  BizTemplateCode: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+    private cookieService: CookieService
+  ) {
     this.route.queryParams.subscribe(params => {
       if (params['BizTemplateCode'] != null) {
         this.BizTemplateCode = params['BizTemplateCode'];
@@ -27,38 +38,70 @@ export class SurveyPagingXComponent implements OnInit {
   }
 
   ngOnInit() {
-      this.ActCode = 'SURVEY_';
-    this.inputPagingObj = new UcpagingModule();
-    this.inputPagingObj._url = "./assets/ucpaging/survey/searchSurvey.json";
-    this.inputPagingObj.enviromentUrl = environment.losUrl;
-    this.inputPagingObj.apiQryPaging = "/v1" + URLConstant.GetPagingObjectBySQL;
-    this.inputPagingObj.pagingJson = "./assets/ucpaging/survey/searchSurvey.json";
+    this.inputPagingObj = new UcPagingObj();
+    let arrCrit = new Array();
+    let critObj: CriteriaObj;
 
-    this.inputPagingObj.ddlEnvironments = [
-      {
-        name: "A.ORI_OFFICE_CODE",
-        environment: environment.FoundationR3Url
-      },
-      {
-        name: "TASK_CLAIM_STAT",
-        environment: environment.FoundationR3Url
-      }
-    ];
+    if(environment.isCore){
+      const UserAccess = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+      this.inputPagingObj._url = "./assets/impl/ucpaging/survey/V2/searchSurveyV2.json";
+      this.inputPagingObj.pagingJson = "./assets/impl/ucpaging/survey/V2/searchSurveyV2.json";
+      this.inputPagingObj.isJoinExAPI = true;
 
-    var arrCrit = new Array();
-    
-    var critObj = new CriteriaObj();
-    critObj.restriction = AdInsConstant.RestrictionLike;
-    critObj.propName = 'WF.ACT_CODE';
-    critObj.value = this.ActCode+this.BizTemplateCode;
-    arrCrit.push(critObj);
+      //Request Obj
+      this.RequestTaskModel.ProcessKey = "WF_CRP_MD_" + this.BizTemplateCode;
+      this.RequestTaskModel.TaskDefinitionKey = 'SURVEY_'+this.BizTemplateCode;
+      // this.RequestTaskModel.OfficeCode = UserAccess[CommonConstant.OFFICE_CODE];
+      // this.RequestTaskModel.RoleCode = UserAccess[CommonConstant.ROLE_CODE];
+      this.RequestTaskModel.OfficeRoleCodes = [UserAccess[CommonConstant.ROLE_CODE], UserAccess[CommonConstant.OFFICE_CODE]];
 
-    this.inputPagingObj.addCritInput = arrCrit;
+      //Integration Obj
+      this.IntegrationObj.baseUrl = URLConstant.GetAllTaskWorkflow;
+      this.IntegrationObj.leftColumnToJoin = "AppNo";
+      this.IntegrationObj.rightColumnToJoin = "ProcessInstanceBusinessKey";
+
+      this.IntegrationObj.requestObj = this.RequestTaskModel;
+      this.inputPagingObj.integrationObj = this.IntegrationObj;
+
+      critObj = new CriteriaObj();
+      critObj.DataType = 'text';
+      critObj.restriction = AdInsConstant.RestrictionEq;
+      critObj.propName = 'a.APP_CURR_STEP';
+      critObj.value = 'SRVY_VRF';
+      arrCrit.push(critObj);
+      this.inputPagingObj.addCritInput = arrCrit;
+
+    }else{
+      this.inputPagingObj._url = "./assets/ucpaging/survey/searchSurvey.json";
+      this.inputPagingObj.pagingJson = "./assets/ucpaging/survey/searchSurvey.json";
+      this.inputPagingObj.apiQryPaging = "/v1" + URLConstant.GetPagingObjectBySQL;
+
+      this.inputPagingObj.ddlEnvironments = [
+        {
+          name: "A.ORI_OFFICE_CODE",
+          environment: environment.FoundationR3Url
+        },
+        {
+          name: "TASK_CLAIM_STAT",
+          environment: environment.FoundationR3Url
+        }
+      ];
+
+      critObj = new CriteriaObj();
+      critObj.restriction = AdInsConstant.RestrictionLike;
+      critObj.propName = 'WF.ACT_CODE';
+      critObj.value = 'SURVEY_'+this.BizTemplateCode;
+      arrCrit.push(critObj);
+      this.inputPagingObj.addCritInput = arrCrit;
+    }
+
+
+
   }
 
   GetCallBack(ev: any){
     if(ev.Key == "ViewProdOffering"){
-      AdInsHelper.OpenProdOfferingViewByCodeAndVersion( ev.RowObj.ProdOfferingCode, ev.RowObj.ProdOfferingVersion);   
+      AdInsHelper.OpenProdOfferingViewByCodeAndVersion( ev.RowObj.ProdOfferingCode, ev.RowObj.ProdOfferingVersion);
     }
   }
 
