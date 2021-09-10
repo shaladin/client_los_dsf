7d08a7492a8d554c322b5c49881789214768de8d
Irwan Santoso <irwan.santoso@ad-ins.com>
2021-09-10 13:49:09 +0700

    Ketinggalan merge

--- src/app/impl/shared/constant/URLConstantX.ts
+++ src/app/impl/shared/constant/URLConstantX.ts
@@ -167,7 +167,7 @@ export class URLConstantX {
   public static GetPagingObjectR2BySQL = "/GenericX/GetPagingObjectR2BySQL";
 
   //SURVEY VERIF X
-  public static SubmitReturnHandlingCommRsvFundV2 = environment.losUrl + "/v2" + "/SurveyVerifX/SubmitReturnHandlingSurveyVerif";
+  public static SubmitReturnHandlingSurveyVerif = environment.losUrl + "/v2" + "/SurveyVerifX/SubmitReturnHandlingSurveyVerif";
 
   // App Credit Review
   public static CrdRvwMakeNewApprovalV2 = environment.losUrl + "/v2" + "/AppCrdRvwX/CrdRvwMakeNewApproval";
