95b4c3b17a0627c3c97550c6eee4c982e6280fc7
muhammad.sa <muhammad.sa@ad-ins.com>
2021-09-14 10:15:22 +0700

    SITDSFCFRTHREE-153: mou doc printing

--- src/app/MOU/customer-doc-printing/customer-doc-printing-detail/customer-doc-printing-detail.component.ts
+++ src/app/MOU/customer-doc-printing/customer-doc-printing-detail/customer-doc-printing-detail.component.ts
@@ -6,6 +6,11 @@ import { URLConstant } from 'app/shared/constant/URLConstant';
 import { CommonConstant } from 'app/shared/constant/CommonConstant';
 import { NavigationConstant } from 'app/shared/constant/NavigationConstant';
 import { MouCustDocPrintForViewObj } from 'app/shared/model/MouCustDocPrintForViewObj.model';
+import { GenericObj } from 'app/shared/model/Generic/GenericObj.Model';
+import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
+import { CookieService } from 'ngx-cookie';
+import { RdlcReportObj, ReportParamObj } from 'app/shared/model/library/RdlcReportObj.model';
+import { AdInsHelper } from 'app/shared/AdInsHelper';
 
 @Component({
   selector: 'app-customer-doc-printing-detail',
@@ -16,12 +21,15 @@ export class CustomerDocPrintingDetailComponent implements OnInit {
   MouCustId: number;
   responseObj: Array<MouCustDocPrintForViewObj> = new Array<MouCustDocPrintForViewObj>();
   mouCustObj: MouCustObj;
+  RdlcReport: RdlcReportObj = new RdlcReportObj();
   readonly CancelLink: string = NavigationConstant.MOU_CUST_DOC_PAGING;
 
   constructor(
     private http: HttpClient,
     private route: ActivatedRoute,
     private router: Router,
+    private toastr: NGXToastrService,
+    private cookieService: CookieService
   ) {
     this.route.queryParams.subscribe(params => {
       if (params['MouCustId'] != null) {
@@ -33,7 +41,13 @@ export class CustomerDocPrintingDetailComponent implements OnInit {
   ngOnInit(): void {
     this.mouCustObj = new MouCustObj();
     this.mouCustObj.MouCustId = this.MouCustId;
-    this.http.post(URLConstant.GetListMouCustDocPrintForViewByMouCustId, { Id: this.MouCustId }).subscribe(
+    this.GetListMouCustDocByMouCustId();
+  }
+
+  GetListMouCustDocByMouCustId(){
+    let reqByIdObj: GenericObj = new GenericObj();
+    reqByIdObj.Id = this.MouCustId;
+    this.http.post(URLConstant.GetListMouCustDocByMouCustId, reqByIdObj).subscribe(
       response => {
         this.responseObj = response[CommonConstant.ReturnObj];
       },
@@ -43,32 +57,53 @@ export class CustomerDocPrintingDetailComponent implements OnInit {
     );
   }
 
-  searchRowVersion(MouCustDocPrintId) {
-    for (var i = 0; i < this.responseObj.length; i++) {
-      if (this.responseObj[i]["MouCustDocPrintId"] == MouCustDocPrintId) {
-        return this.responseObj[i]["RowVersion"];
-      }
+  Print(item) {
+    try {
+      let currentUserContext = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+      this.RdlcReport.RequestingUsername = currentUserContext.UserName;
+      this.RdlcReport.ReportInfo.ReportName = item.MouDocName;
+      this.RdlcReport.ReportInfo.ReportTemplateCode = item.RptTmpltCode;
+      this.RdlcReport.ReportInfo.ReportParameters = new Array<ReportParamObj>();
+      this.RdlcReport.ReportInfo.ExportFormat = 0;
+
+      let reportParamObj: ReportParamObj = new ReportParamObj();
+      reportParamObj.paramKey = "MouCustId";
+      reportParamObj.paramValue = this.MouCustId.toString();
+      reportParamObj.paramAssignment = 1;
+      this.RdlcReport.ReportInfo.ReportParameters.push(reportParamObj);
+
+      // this.RdlcReport.MainReportInfoDetail.ReportDataProviderParameter["RptTmpltCode"] = item.RptTmpltCode;
+
+      this.http.post(URLConstant.GenerateReportR3, this.RdlcReport).subscribe(
+        (response) => {
+          let linkSource: string = 'data:application/pdf;base64,' + response["ReportFile"];
+          let fileName: string = item.MouDocName + ".pdf";
+          const downloadLink = document.createElement("a");
+          downloadLink.href = linkSource;
+          downloadLink.download = fileName;
+
+          if (response["ReportFile"] != undefined) {
+            downloadLink.click();
+            this.SaveAgrmntDocPrint(item.MouCustDocId);
+            this.toastr.successMessage(response['message']);
+          }
+          else {
+            this.toastr.warningMessage(response['message']);
+          }
+        }
+      );
     }
-    return null;
+    catch (error) {
+      this.toastr.warningMessage(error);
+    }
   }
 
-  print(MouCustDocPrintId) {
-    var mouObj = { Id: MouCustDocPrintId, RowVersion: this.searchRowVersion(MouCustDocPrintId) };
-    this.http.post(URLConstant.EditMouCustDocPrintSequenceNo, mouObj).subscribe(
-      response => {
-        var mouCustObj = { Id: this.MouCustId };
-        this.http.post(URLConstant.GetListMouCustDocPrintForViewByMouCustId, mouCustObj).subscribe(
-          response => {
-            this.responseObj = response[CommonConstant.ReturnObj];
-          },
-          () => {
-            this.router.navigateByUrl('Error');
-          }
-        );
-      },
+  SaveAgrmntDocPrint(mouCustDocId) {
+    let reqByIdObj: GenericObj = new GenericObj();
+    reqByIdObj.Id = mouCustDocId
+    this.http.post(URLConstant.AddMouCustDocPrint, reqByIdObj).subscribe(
       () => {
-        this.router.navigateByUrl('Error');
-      }
-    );
+        this.GetListMouCustDocByMouCustId();
+      });
   }
 }
