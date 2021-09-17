7981b6e3591d805f5b0439e68c19464e2c0e2e03
Edward Bala Yansen <edward.by@ad-ins.com>
2021-09-17 13:10:04 +0700

    settingan kurang utk view dms

--- src/app/NEW-NAP/business-process/admin-process/purchase-order/purchase-order/purchase-order.component.ts
+++ src/app/NEW-NAP/business-process/admin-process/purchase-order/purchase-order/purchase-order.component.ts
@@ -124,6 +124,7 @@ export class PurchaseOrderComponent implements OnInit {
   
           this.dmsAppObj.MetadataObject.push(new DMSLabelValueObj(CommonConstant.DmsNoApp, this.appNo));
   
+          this.dmsAppObj.Option.push(new DMSLabelValueObj(CommonConstant.DmsOverideSecurity, CommonConstant.DmsOverideUploadView));
           this.dmsObj.Option.push(new DMSLabelValueObj(CommonConstant.DmsOverideSecurity, CommonConstant.DmsOverideUploadView));
           if (mouId != null && mouId != "") {
             this.http.post(URLConstant.GetMouCustById, { Id: mouId }).subscribe(
