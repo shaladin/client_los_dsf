4df1c9e9e47687190299b3565a261cfaa2bcd1c1
Edward Bala Yansen <edward.by@ad-ins.com>
2021-09-20 16:33:08 +0700

    hapus log

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,27 +4,27 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   navbarColor: 'transparent',
   //losUrl: 'http://localhost:5001',
-  //FoundationR3Url: 'http://localhost:5000',
-  losUrl: 'http://r3app-server.ad-ins.com/LOS',
-  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
-  DMSUrl: "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
-  AMSUrl: "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
-  LMSUrl: "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
-  ApprovalR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_R3/Approval',
+  // FoundationR3Url: 'http://localhost:5000',
+  losUrl: 'http://r3app-server.ad-ins.com/LOS_DEV',
+  FoundationR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
+  DMSUrl : "http://sky.ad-ins.com/LiteDMS_POC/Integration/ViewDoc.aspx",
+  AMSUrl : "http://r3app-server.ad-ins.com/AMS_DEMO", //OPL dah ada
+  LMSUrl : "http://r3app-server.ad-ins.com/LMS_DEMO", //OPL dah ada
+  ApprovalR3Url: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV/Approval',
   ApprovalR3OplUrl: 'http://r3impl-appsvr.ad-ins.com/APPROVAL_OPL_BE_R3/',
-  FoundationR3Web: 'http://r3web-server.ad-ins.com/Foundation',
-  losR3Web: 'http://r3web-server.ad-ins.com/LOSR3',
+  FoundationR3Web: 'http://r3web-server.ad-ins.com/FOUNDATION_DEV',
+  losR3Web: 'http://r3web-server/LOS_DEV',
   lmsR3web : "http://r3impl-websvr.ad-ins.com/LMS",
   WorkflowR3Url: 'http://R3App-Server.ad-ins.com/WORKFLOW_R3',
-  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_R3',
+  WebSocketURL: 'http://r3app-server.ad-ins.com/FOUNDATION_DEV',
   DashboardURL: 'http://r3app-server.ad-ins.com/Dashboard',
   dmsURL: 'http://kfx-svr/LITEDMS_OPL/LiteDMS/pageconfins.aspx',
   WfR3Url: 'http://r3impl-appsvr.ad-ins.com/WORKFLOW_OPL/',
   ChipperKeyLocalStorage: "AdInsFOU2020OKOK", // 256 bit atau 16 karakter
   ChipperKeyCookie: "AdInsFOU12345678", // 256 bit atau 16 karakter & harus sama dengan BE
   Module: "LOS",
-  isCore : false
+  isCore : true
 };
