a76cbe1616b2cd393ac5041a6e4db8d5a8e169f4
hansen.christian <hansen.christian@ad-ins.com>
2021-10-01 17:19:52 +0700

    Sweep office, role paging

--- src/app/NEW-NAP/business-process/additional-process/return-handling/return-handling-edit-nap4/return-handling-edit-nap4.component.ts
+++ src/app/NEW-NAP/business-process/additional-process/return-handling/return-handling-edit-nap4/return-handling-edit-nap4.component.ts
@@ -46,9 +46,7 @@ export class ReturnHandlingEditNap4Component implements OnInit {
       this.inputPagingObj.isJoinExAPI = true
       
       this.RequestTaskModel.ProcessKey = CommonConstant.RTN_EDIT_NAP4 + this.BizTemplateCode;
-      this.RequestTaskModel.OfficeCode = UserAccess[CommonConstant.OFFICE_CODE];
       this.RequestTaskModel.TaskDefinitionKey = CommonConstant.EDIT_NAP4 + this.BizTemplateCode;
-      this.RequestTaskModel.RoleCode = UserAccess[CommonConstant.ROLE_CODE];
       this.RequestTaskModel.OfficeRoleCodes = [UserAccess[CommonConstant.ROLE_CODE],
                                                 UserAccess[CommonConstant.OFFICE_CODE],
                                                 UserAccess[CommonConstant.ROLE_CODE] + "-" + UserAccess[CommonConstant.OFFICE_CODE]];
