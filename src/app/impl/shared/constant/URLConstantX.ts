a1c70d972c834c96bb36fb51755681322d0d943b
Andre Sucipto <andre.sucipto@ad-ins.com>
2021-10-08 18:43:54 +0700

    IMPL-29979, penambahan DMS pada cessie PGL

--- src/app/impl/shared/constant/URLConstantX.ts
+++ src/app/impl/shared/constant/URLConstantX.ts
@@ -73,6 +73,7 @@ export class URLConstantX {
   public static SubmitPreGoLiveCessieV2 = environment.losUrl + "/v2" + "/Cessie/SubmitPreGoLive";
   public static SubmitCancel = environment.losUrl + "/v1" + "/Cessie/SubmitCancel";
   public static SubmitCancelV2 = environment.losUrl + "/v2" + "/Cessie/SubmitCancelV2";
+  public static GetInitForDmsDataByCessieHXId = environment.losUrl + "/v1" + "/Cessie/GetInitForDmsDataByCessieHXId";
 
   //MOU CUST X
   public static GetMouCustByAppIdX = environment.losUrl +'/v1' +  "/MouCustX/GetMouCustByAppId";
