a8c20756e4538c0202aa1e2afa0416f40a67fe79
Edward Bala Yansen <edward.by@ad-ins.com>
2021-10-18 16:44:37 +0700

    LOSRTHREE-887 - view financial di agrmntActivation

--- src/app/shared/model/Response/NAP/AgrAct/ResAgrmntActivationFinDataAndFeeObj.model.ts
+++ src/app/shared/model/Response/NAP/AgrAct/ResAgrmntActivationFinDataAndFeeObj.model.ts
@@ -10,10 +10,12 @@ export class ResAgrmntActivationFinDataAndFeeObj {
 
 export class ResAppFeeObj {
     AppFeeAmt : number;
+    CapitalizedAmt : number;
     FeeName : string;
 
     constructor(){
         this.AppFeeAmt = 0;
+        this.CapitalizedAmt = 0;
         this.FeeName = "";
     }
 }
@@ -23,15 +25,35 @@ export class ResponseAppFinDataObj {
     TotalDownPaymentNettAmt : number;
     NtfAmt : number;
     TotalInsCustAmt : number;
+    TotalLifeInsCustAmt : number;
+    TotalFeeCptlzAmt : number;
     InsCptlzAmt : number;
     InstAmt : number;
+    TotalFeeAmt : number;
+    LifeInsCptlzAmt : number;
+    DownPaymentGrossAmt : number;
+    DownPaymentNettAmt : number;
+    MrFirstInstTypeCode : string;
+    MrFirstInstTypeName : string;
+    MrInstSchemeCode : string;
+    MrInstSchemeName : string;
 
     constructor(){
         this.TotalAssetPriceAmt = 0;
         this.TotalDownPaymentNettAmt = 0;
         this.NtfAmt = 0;
+        this.TotalFeeAmt = 0;
         this.TotalInsCustAmt = 0;
+        this.TotalLifeInsCustAmt = 0;
+        this.TotalFeeCptlzAmt = 0;
         this.InsCptlzAmt = 0;
+        this.LifeInsCptlzAmt = 0;
         this.InstAmt = 0;
+        this.DownPaymentGrossAmt = 0;
+        this.DownPaymentNettAmt = 0;
+        this.MrFirstInstTypeCode = "";
+        this.MrFirstInstTypeName = "";
+        this.MrInstSchemeCode = "";
+        this.MrInstSchemeName = "";
     }
 }
