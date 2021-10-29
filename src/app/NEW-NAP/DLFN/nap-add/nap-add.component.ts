51ee0cdf126062952e34e1b9e9b7df76e40292a5
calvin <calvin.cen@ad-ins.com>
2021-10-29 15:15:30 +0700

    Cekin Fixing Error DLFN

--- src/app/NEW-NAP/DLFN/nap-add/nap-add.component.ts
+++ src/app/NEW-NAP/DLFN/nap-add/nap-add.component.ts
@@ -119,7 +119,7 @@ export class NapAddComponent implements OnInit {
 
     this.inputLookupObjName = new InputLookupObj();
     this.inputLookupObjName.urlJson = "./assets/uclookup/NAP/lookupAppName.json";
-    this.inputLookupObjName.urlEnviPaging = environment.FoundationR3Url + "/v1";
+    this.inputLookupObjName.urlEnviPaging = environment.losUrl + "/v1";
     this.inputLookupObjName.pagingJson = "./assets/uclookup/NAP/lookupAppName.json";
     this.inputLookupObjName.genericJson = "./assets/uclookup/NAP/lookupAppName.json";
     this.inputLookupObjName.nameSelect = this.NapAppForm.controls.ProdOfferingName.value;
