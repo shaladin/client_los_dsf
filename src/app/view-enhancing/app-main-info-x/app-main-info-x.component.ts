6797b38b6e75515e8b56e3609c801651742a40f5
Timothy Gracia Yaputra <timothy.gy@ad-ins.com>
2021-12-03 13:21:21 +0700

    CR - UATDSFCF-380 - Perubahan Label Customer Name menjadi Hyperlink ke Customer View

---
+++ src/app/view-enhancing/app-main-info-x/app-main-info-x.component.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { UcViewGenericObj } from 'app/shared/model/uc-view-generic-obj.model';
+import { HttpClient } from '@angular/common/http';
+import { URLConstant } from 'app/shared/constant/URLConstant';
+import { CommonConstant } from 'app/shared/constant/CommonConstant';
+import { AdInsHelper } from 'app/shared/AdInsHelper';
+import { UcviewgenericComponent } from '@adins/ucviewgeneric';
+import { TranslateService } from '@ngx-translate/core';
+import { ActivatedRoute } from '@angular/router';
+import { AdInsHelperService } from 'app/shared/services/AdInsHelper.service';
+
+@Component({
+  selector: 'app-app-main-info-x',
+  templateUrl: './app-main-info-x.component.html'
+})
+export class AppMainInfoXComponent implements OnInit {
+  private viewGeneric: UcviewgenericComponent;
+  whereValue = [];
+  @ViewChild('viewGeneric') set content(content: UcviewgenericComponent) {
+    if (content) { // initially setter gets called with undefined
+      this.viewGeneric = content;
+    }
+  }
+  viewGenericObj: UcViewGenericObj = new UcViewGenericObj();
+  @Input() AppId: number;
+  AppNo: number;
+  AppObj: any;
+
+  constructor(
+    private http: HttpClient, 
+    private translateService: TranslateService, 
+    private route: ActivatedRoute,
+    private adInsHelperService: AdInsHelperService) {
+    this.route.queryParams.subscribe(params => {
+      if(params["AppId"] == "undefined"){
+        this.AppNo = params["AppNo"]
+
+      }else{
+        this.AppId = params["AppId"];
+
+      }
+    })
+   }
+
+  ngOnInit() {
+    this.translateService.use(localStorage.getItem('lang') || 'en');
+    this.http.post(URLConstant.GetAppById, { Id: this.AppId }).subscribe(
+      (response) => {
+        this.AppObj = response;
+        if (this.AppObj.BizTemplateCode == CommonConstant.CF4W) {
+          this.viewGenericObj.viewInput = "./assets/impl/ucviewgeneric/viewNapAppMainInformationX.json";
+        }
+        else if (this.AppObj.BizTemplateCode == CommonConstant.FL4W) {
+          this.viewGenericObj.viewInput = "./assets/impl/ucviewgeneric/viewNapAppFL4WMainInformationX.json";
+        }
+        else if (this.AppObj.BizTemplateCode == CommonConstant.CFNA) {
+          this.viewGenericObj.viewInput = "./assets/impl/ucviewgeneric/viewAppMainInfoCFNAX.json";
+        }
+        else if (this.AppObj.BizTemplateCode == CommonConstant.OPL) {
+          this.viewGenericObj.viewInput = "./assets/ucviewgeneric/opl/view-opl-main-info.json";
+        }
+        else if (this.AppNo != null){
+          this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewAppMainInfoByAppNo.json";
+        }else{
+          this.viewGenericObj.viewInput = "./assets/impl/ucviewgeneric/viewAppMainInfoX.json";
+        }
+      }
+    );
+  }
+
+  ReloadUcViewGeneric() {
+    this.viewGeneric.initiateForm();
+  }
+
+  GetCallBack(ev: any) {
+    if (ev.Key == "ViewProdOffering") {
+      AdInsHelper.OpenProdOfferingViewByCodeAndVersion(ev.ViewObj.ProdOfferingCode, ev.ViewObj.ProdOfferingVersion);
+    } 
+    else if (ev.Key == "HighlightComment") {
+      var link: string;
+      var custObj = { CustNo: ev.ViewObj.CustNo };
+      this.http.post(URLConstant.GetCustByCustNo, custObj).subscribe(
+        response => {
+          if(response["MrCustTypeCode"] == CommonConstant.CustTypePersonal){
+            this.adInsHelperService.OpenCustomerViewByCustId(response["CustId"]);
+          }
+          if(response["MrCustTypeCode"] == CommonConstant.CustTypeCompany){
+            this.adInsHelperService.OpenCustomerCoyViewByCustId(response["CustId"]);
+          }
+        }
+      );
+    }
+    else if (ev.Key == "Customer") {
+        var custObj = {
+          CustNo: ev.ViewObj.CustNo
+        };
+        this.http.post(URLConstant.GetCustByCustNo, custObj).subscribe(
+          (response) => {
+            if(response['MrCustTypeCode'] == CommonConstant.CustTypePersonal) {
+              AdInsHelper.OpenCustomerViewByCustId(response['CustId']);
+            } else if(response['MrCustTypeCode'] == CommonConstant.CustTypeCompany) {
+              AdInsHelper.OpenCustomerCoyViewByCustId(response['CustId']);
+            }
+          }
+        )
+    }
+  }
+}
