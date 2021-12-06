b017a222a44a3136050027c015f0ae356be2ac6e
calvin <calvin.cen@ad-ins.com>
2021-12-06 09:45:53 +0700

    LOSRTHREE-1172 -Add Mou View Cust

---
+++ src/app/view-enhancing/mou-view/mou-view-customer/mou-view-customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mou-view-customer',
+  templateUrl: './mou-view-customer.component.html'
+})
+export class MouViewCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
