29bcf06fce2e7944d2bef1295bfac9cb10f114ab
Xania Yoan <xania.ya@ad-ins.com>
2021-12-20 09:56:10 +0700

    LOSRTHREE-1174 : Selain Office Type HO, maka akan di redirect ke halaman Unauthorized

--- src/app/product/product-ho/prod-ho-deact/prod-ho-deact-paging/prod-ho-deact-paging.component.ts
+++ src/app/product/product-ho/prod-ho-deact/prod-ho-deact-paging/prod-ho-deact-paging.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from "@angular/core";
 import { AdInsConstant } from "app/shared/AdInstConstant";
 import { UcPagingObj } from "app/shared/model/uc-paging-obj.model";
 import { CriteriaObj } from "app/shared/model/criteria-obj.model";
+import { AdInsHelper } from "app/shared/AdInsHelper";
+import { CommonConstant } from "app/shared/constant/CommonConstant";
+import { NavigationConstant } from "app/shared/constant/NavigationConstant";
+import { Router } from "@angular/router";
+import { CookieService } from "ngx-cookie";
 @Component({
   selector: 'app-prod-ho-deact-paging',
   templateUrl: './prod-ho-deact-paging.component.html'
@@ -10,9 +15,16 @@ export class ProdHoDeactPagingComponent implements OnInit {
   InputPagingObj: UcPagingObj = new UcPagingObj();
   ArrCrit: Array<CriteriaObj> = new Array<CriteriaObj>();
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private cookieService: CookieService) { }
 
   ngOnInit() {
+    let context = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+    if (context[CommonConstant.MR_OFFICE_TYPE_CODE] != CommonConstant.HeadOffice) {
+      this.router.navigate([NavigationConstant.PROD_HO_UNAUTHORIZED], { queryParams: {}, skipLocationChange: false });
+    }
+    
     this.InputPagingObj._url = "./assets/ucpaging/product/searchProductHODeactivate.json";
     this.InputPagingObj.pagingJson = "./assets/ucpaging/product/searchProductHODeactivate.json";
 
