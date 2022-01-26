215fd1f2f190d1d4f22d47555936cafd942bcbdf
Xania Yoan <xania.ya@ad-ins.com>
2022-01-26 14:03:21 +0700

    LOSRTHREE-1368 : Commission Reserved Fund - Bank Account Supplier and Supplier Employee

--- src/app/NEW-NAP/business-process/credit-process/commission-reserved-fund/component/commission-v3/form-commission-generate-v2/form-commission-generate-v2.component.ts
+++ src/app/NEW-NAP/business-process/credit-process/commission-reserved-fund/component/commission-v3/form-commission-generate-v2/form-commission-generate-v2.component.ts
@@ -295,7 +295,7 @@ export class FormCommissionGenerateV2Component implements OnInit {
               Value: response["ReturnObject"][i]["BankAccountName"],
               BankCode: response["ReturnObject"][i]["BankCode"],
               BankName: response["ReturnObject"][i]["BankName"],
-              BankBranch: ""
+              BankBranch: response["ReturnObject"][i]["BankBranch"]
             }) as FormGroup;
             this.parentForm.controls[this.identifier]["controls"][idx].controls.DropDownList.push(eachDDLDetail);
           }
@@ -306,7 +306,7 @@ export class FormCommissionGenerateV2Component implements OnInit {
       let ReqGetListBankObj: ReqGetListBankByVendorEmpNoAndCodeObj = new ReqGetListBankByVendorEmpNoAndCodeObj();
       ReqGetListBankObj.VendorEmpNo = code;
       ReqGetListBankObj.VendorCode = this.parentForm.value[this.identifier][idx].SupplCode;
-      this.http.post<VendorBankAccObj>(URLConstant.GetListBankByVendorEmpNoAndVendorCode, ReqGetListBankObj).subscribe(
+      this.http.post<VendorBankAccObj>(URLConstant.GetListActiveBankByVendorEmpNoAndVendorCode, ReqGetListBankObj).subscribe(
         (response) => {
           var len = response["ReturnObject"].length;
           for (var i = 0; i < len; i++) {
@@ -316,7 +316,7 @@ export class FormCommissionGenerateV2Component implements OnInit {
               Value: response["ReturnObject"][i]["BankAccountName"],
               BankCode: response["ReturnObject"][i]["BankCode"],
               BankName: response["ReturnObject"][i]["BankName"],
-              BankBranch: ""
+              BankBranch: response["ReturnObject"][i]["BankBranch"]
             }) as FormGroup;
             this.parentForm.controls[this.identifier]["controls"][idx].controls.DropDownList.push(eachDDLDetail);
           }
