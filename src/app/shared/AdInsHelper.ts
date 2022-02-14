b11596a3fb553fd70bdb2f6dcf8881c137b90b8e
calvin <calvin.cen@ad-ins.com>
2022-02-14 09:24:50 +0700

    Fixing Cannot Login LOS

--- src/app/shared/AdInsHelper.ts
+++ src/app/shared/AdInsHelper.ts
@@ -247,7 +247,7 @@ export class AdInsHelper {
   }
 
   public static SetCookie(cookieService: CookieService, key: string, value: string) {
-    cookieService.put(key, this.EncryptString(value, environment.ChipperKeyCookie), {secure : true, sameSite : "strict"});
+    cookieService.put(key, this.EncryptString(value, environment.ChipperKeyCookie), {secure : true});
   }
 
   public static GetCookie(cookieService: CookieService, key: string) {
