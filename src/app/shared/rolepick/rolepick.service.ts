26f31f38868a8983e1b32fde376e177bc01211c9
shafiq.affandi <muhammad.sa@ad-ins.com>
2022-02-18 13:29:06 +0700

    ubah ui login & rolepick ke versi lama

--- src/app/shared/rolepick/rolepick.service.ts
+++ src/app/shared/rolepick/rolepick.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { MatDialog, MatDialogConfig } from '@angular/material';
+import { MatDialog } from '@angular/material';
 import { RolepickComponent } from 'app/shared/rolepick/rolepick.component';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from 'environments/environment';
 import { Router } from '@angular/router';
 import { AdInsHelper } from '../AdInsHelper';
+import { URLConstant } from '../constant/URLConstant';
 import { CommonConstant } from '../constant/CommonConstant';
 import { NavigationConstant } from '../constant/NavigationConstant';
 import { CookieService } from 'ngx-cookie';
@@ -17,19 +18,25 @@ export class RolePickService {
         private router: Router, private cookieService: CookieService) { }
     openDialog(data, type = ""): void {
         if (type == "modal") {
-            let UserAccess = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
-            this.http.post(AdInsConstant.GetListJobTitleByUsernameAndModuleV2, {UserName : UserAccess['UserName'], Module : environment.Module}, AdInsConstant.SpinnerOptions).subscribe(
+            var roleObject2 = {
+                RequestDateTime: AdInsHelper.GetCookie(this.cookieService, CommonConstant.BUSINESS_DATE_RAW),
+                RowVersion: ""
+            };
+
+            this.http.post(AdInsConstant.LoginByTokenV2, roleObject2).subscribe(
                 (response) => {
                     const object = {
-                        response: response
+                        response: response["ReturnObject"]
                     };
 
-                    const dialogConfig = new MatDialogConfig();
-                    dialogConfig.id = 'role-modal';
-                    dialogConfig.width = '45%';
-                    dialogConfig.data = object;
-                    dialogConfig.backdropClass = "blur-bg";
-                    const dialogRef = this.dialog.open(RolepickComponent, dialogConfig);
+                    const dialogRef = this.dialog.open(RolepickComponent, {
+                        id: 'role-modal',
+                        width: '85%',
+                        position: {
+                            top: '12px'
+                        },
+                        data: object
+                    });
 
                     dialogRef.afterClosed().subscribe(() => {
                     });
@@ -38,45 +45,41 @@ export class RolePickService {
 
 
         } else {
-            if (data.response.RefUserRoles.length == 1 && type == "") {
-                var item = data.response;
+            if (data.response.length == 1 && type == "") {
+                var item = data.response[0];
                 var UserIdentityObj = {
                     RefUserId: item.RefUserId,
-                    UserName: item.Username,
+                    UserName: item.UserName,
                     EmpNo: item.EmpNo,
                     EmpName: item.EmpName,
-                    OfficeId: item.RefUserRoles[0].RefOfficeId,
-                    OfficeCode: item.RefUserRoles[0].OfficeCode,
-                    OfficeName: item.RefUserRoles[0].OfficeName,
-                    MrOfficeTypeCode: item.RefUserRoles[0].MrOfficeTypeCode,
-                    RoleId: item.RefUserRoles[0].Roles[0].RefRoleId,
-                    RoleCode: item.RefUserRoles[0].Roles[0].RoleCode,
-                    RoleName: item.RefUserRoles[0].Roles[0].RoleName,
-                    JobTitleId: item.RefUserRoles[0].Roles[0].RefJobTitleId,
-                    JobTitleCode: item.RefUserRoles[0].Roles[0].JobTitleCode,
-                    JobTitleName: item.RefUserRoles[0].Roles[0].JobTitleName,
+                    OfficeId: item.RefOfficeId,
+                    OfficeCode: item.OfficeCode,
+                    OfficeName: item.OfficeName,
+                    MrOfficeTypeCode: item.MrOfficeTypeCode,
+                    RoleId: item.RefRoleId,
+                    RoleCode: item.RoleCode,
+                    RoleName: item.RoleName,
+                    JobTitleId: item.RefJobTitleId,
+                    JobTitleCode: item.JobTitleCode,
+                    JobTitleName: item.JobTitleName,
                     BusinessDt: item.BusinessDt,
                     BusinessDtStr: item.BusinessDtStr,
                     Email: item.Email1,
                     CoyName: item.CoyName
-                }
-                let roleObject = {
+                  }
+
+                var roleObject = {
                     UserName: data.user,
                     Password: data.pwd,
                     OfficeCode: item.OfficeCode,
                     RoleCode: item.RoleCode,
                     JobTitleCode: item.JobTitleCode,
                     RequestDateTime: item.BusinessDt,
+                    ModuleCode: "LOS",
                     RowVersion: "",
-                    Ip: "",
-                    ModuleCode: environment.Module,
                     UserIdentityObj: UserIdentityObj
                 };
-                let SpinnerHeaders = new HttpHeaders({
-                    'IsLoading': "true"
-                });
-                let SpinnerOptions = { headers: SpinnerHeaders, withCredentials: true };
-                this.http.post(AdInsConstant.LoginByRoleV2, roleObject, SpinnerOptions).subscribe(
+                this.http.post(AdInsConstant.LoginByRoleV2, roleObject, { withCredentials: true }).subscribe(
                     (response) => {
                         //Cookie sudah diambil dari BE (Di set manual dulu)
 
@@ -88,24 +91,23 @@ export class RolePickService {
                         AdInsHelper.SetCookie(this.cookieService, "Username", JSON.stringify(response["Identity"]["UserName"]));
                         AdInsHelper.SetLocalStorage(CommonConstant.ENVIRONMENT_MODULE, environment.Module);
 
-                        this.http.post(AdInsConstant.GetAllActiveRefFormByRoleCodeAndModuleCode, {RoleCode: item.RefUserRoles[0].Roles[0].RoleCode, ModuleCode: environment.Module}, { withCredentials: true }).subscribe(
+                        this.http.post(AdInsConstant.GetAllActiveRefFormByRoleCodeAndModuleCode, { RoleCode: item.RoleCode, ModuleCode: environment.Module }, { withCredentials: true }).subscribe(
                             (response) => {
                                 AdInsHelper.SetLocalStorage(CommonConstant.MENU, JSON.stringify(response[CommonConstant.ReturnObj]));
                                 this.router.navigate([NavigationConstant.DASHBOARD]);
                             });
-                    }
-                )
+                    });
             }
             //Ini kalau dia ada lebih dari 1 Role, maka buka modal
             else {
-                const dialogConfig = new MatDialogConfig();
-                dialogConfig.disableClose = true;
-                dialogConfig.id = 'role-modal';
-                dialogConfig.width = '45%';
-                dialogConfig.data = data;
-                dialogConfig.backdropClass = "blur-bg";
-                const dialogRef = this.dialog.open(RolepickComponent, dialogConfig);
-
+                const dialogRef = this.dialog.open(RolepickComponent, {
+                    id: 'role-modal',
+                    width: '85%',
+                    position: {
+                        top: '12px'
+                    },
+                    data: data
+                });
 
                 dialogRef.afterClosed().subscribe(() => {
                 });
