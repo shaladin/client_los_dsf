e49a310c97bb97533fcddbf827b75907ba7d7013
Faesal <faesal.hkp@ad-ins.com>
2022-05-17 14:27:55 +0700

    Perbaikan UATDSFCF-401

--- src/app/impl/NEW-NAP/business-process/admin-process/invoice-data/invoice-data-detail/invoice-data-detail-x.component.ts
+++ src/app/impl/NEW-NAP/business-process/admin-process/invoice-data/invoice-data-detail/invoice-data-detail-x.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
-import { CommonConstant } from 'app/shared/constant/CommonConstant';
-import { AdInsHelper } from 'app/shared/AdInsHelper';
-import { NavigationConstant } from 'app/shared/constant/NavigationConstant';
-import { CookieService } from 'ngx-cookie';
-import { URLConstantX } from 'app/impl/shared/constant/URLConstantX';
-import { Validators, FormBuilder } from '@angular/forms';
-import { ResGetAllAssetDataForPOByAsset } from 'app/shared/model/response/purchase-order/res-get-all-asset-data-for-po.model';
-import { PurchaseOrderHObj } from 'app/shared/model/purchase-order-h-obj.model';
-import { ReqAssetDataObj } from 'app/shared/model/request/app-asset/req-app-asset-obj.model';
-import { PurchaseOrderDObj } from 'app/shared/model/purchase-order-d-obj.model';
-import { UcDropdownListObj } from 'app/shared/model/library/uc-dropdown-list-obj.model';
-import { GenericObj } from 'app/shared/model/generic/generic-obj.model';
-import { RefMasterObj } from 'app/shared/model/ref-master-obj.model';
-import { formatDate } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { NGXToastrService } from "app/components/extra/toastr/toastr.service";
+import { CommonConstant } from "app/shared/constant/CommonConstant";
+import { AdInsHelper } from "app/shared/AdInsHelper";
+import { NavigationConstant } from "app/shared/constant/NavigationConstant";
+import { CookieService } from "ngx-cookie";
+import { URLConstantX } from "app/impl/shared/constant/URLConstantX";
+import { Validators, FormBuilder } from "@angular/forms";
+import { ResGetAllAssetDataForPOByAsset } from "app/shared/model/response/purchase-order/res-get-all-asset-data-for-po.model";
+import { PurchaseOrderHObj } from "app/shared/model/purchase-order-h-obj.model";
+import { ReqAssetDataObj } from "app/shared/model/request/app-asset/req-app-asset-obj.model";
+import { PurchaseOrderDObj } from "app/shared/model/purchase-order-d-obj.model";
+import { UcDropdownListObj } from "app/shared/model/library/uc-dropdown-list-obj.model";
+import { GenericObj } from "app/shared/model/generic/generic-obj.model";
+import { RefMasterObj } from "app/shared/model/ref-master-obj.model";
+import { formatDate } from "@angular/common";
 
 @Component({
-  selector: 'app-invoice-data-detail-x',
-  templateUrl: './invoice-data-detail-x.component.html'
+  selector: "app-invoice-data-detail-x",
+  templateUrl: "./invoice-data-detail-x.component.html",
 })
 export class InvoiceDataDetailXComponent implements OnInit {
-
   arrValue: Array<number> = [];
   AgrmntId: number;
   AppId: number;
@@ -33,23 +32,29 @@ export class InvoiceDataDetailXComponent implements OnInit {
   lobCode: string;
   TaskListId: string;
   BizTemplateCode: string;
-  responseRefBank: any
+  responseRefBank: any;
   isReady: boolean = false;
 
   InvoiceDataForm = this.fb.group({
-    SupplCode: [''],
-    InvoiceNo: [Validators.required, Validators.maxLength(20)],
-    InvoiceDt: ['', Validators.required],
-    TotalInvoiceAmt: ['', Validators.required],
-    RcvBy: [''],
-    InvoiceAmt: [''],
-    PurchaseOrderHId: ['']
+    SupplCode: [""],
+    InvoiceNo: ["", [Validators.required, Validators.maxLength(20)]],
+    InvoiceDt: ["", Validators.required],
+    TotalInvoiceAmt: [0, [Validators.required, Validators.min(1)]],
+    RcvBy: [""],
+    InvoiceAmt: [""],
+    PurchaseOrderHId: [""],
   });
 
   readonly CancelLink: string = NavigationConstant.NAP_ADM_PRCS_INVOICE_DATA;
-  constructor(private fb: FormBuilder,
-    private route: ActivatedRoute, private http: HttpClient, private router: Router, private toastr: NGXToastrService, private cookieService: CookieService) {
-    this.route.queryParams.subscribe(params => {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+    private toastr: NGXToastrService,
+    private cookieService: CookieService
+  ) {
+    this.route.queryParams.subscribe((params) => {
       if (params["AgrmntId"] != null) {
         this.AgrmntId = params["AgrmntId"];
       }
@@ -68,7 +73,9 @@ export class InvoiceDataDetailXComponent implements OnInit {
   isDataExist: boolean = false;
   readonly bizTemplateCodeFl4w: string = CommonConstant.FL4W;
   async ngOnInit() {
-    let context = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
+    let context = JSON.parse(
+      AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS)
+    );
     this.arrValue.push(this.AgrmntId);
     this.purchaseOrderHObj = new PurchaseOrderHObj();
 
@@ -76,14 +83,17 @@ export class InvoiceDataDetailXComponent implements OnInit {
     appAssetObj.AppId = this.AppId;
     appAssetObj.AgrmntId = this.AgrmntId;
     appAssetObj.SupplCode = this.SupplCode;
- 
 
-    await this.http.post(URLConstantX.GetPurchaseOrderHByAgrmntIdAndSupplCodeX, appAssetObj).toPromise().then(
-      async (response) => {
+    await this.http
+      .post(URLConstantX.GetPurchaseOrderHByAgrmntIdAndSupplCodeX, appAssetObj)
+      .toPromise()
+      .then(async (response) => {
         if (response["PurchaseOrderHId"] != 0) {
-          this.purchaseOrderHObj.PurchaseOrderHId = response["PurchaseOrderHId"];
+          this.purchaseOrderHObj.PurchaseOrderHId =
+            response["PurchaseOrderHId"];
           this.purchaseOrderHObj.PurchaseOrderNo = response["PurchaseOrderNo"];
-          this.purchaseOrderHObj.TotalPurchaseOrderAmt = response["TotalPurchaseOrderAmt"];
+          this.purchaseOrderHObj.TotalPurchaseOrderAmt =
+            response["TotalPurchaseOrderAmt"];
         }
       });
 
@@ -91,7 +101,8 @@ export class InvoiceDataDetailXComponent implements OnInit {
       SupplCode: this.SupplCode,
       RcvBy: context.UserName,
       PurchaseOrderHId: this.purchaseOrderHObj.PurchaseOrderHId,
-      InvoiceAmt: this.purchaseOrderHObj.TotalPurchaseOrderAmt
+      InvoiceAmt: this.purchaseOrderHObj.TotalPurchaseOrderAmt,
+      TotalInvoiceAmt: this.purchaseOrderHObj.TotalPurchaseOrderAmt,
     });
 
     var RequestInvoiceX = {
@@ -100,69 +111,84 @@ export class InvoiceDataDetailXComponent implements OnInit {
       },
       RequestInvoiceDXObj: {
         PurchaseOrderHId: this.InvoiceDataForm.controls.PurchaseOrderHId.value,
-      }
-    }
+      },
+    };
 
-    await this.http.post(URLConstantX.GetInvoiceXByVendorCodeAndPurchaseOrderHId, RequestInvoiceX).subscribe(
-      (response) => {
+    await this.http
+      .post(
+        URLConstantX.GetInvoiceXByVendorCodeAndPurchaseOrderHId,
+        RequestInvoiceX
+      )
+      .subscribe((response) => {
         console.log(response);
-        if (response['ResponseInvoiceHXObj'] != null) {
+        if (response["ResponseInvoiceHXObj"] != null) {
           this.isDataExist = true;
           this.InvoiceDataForm.patchValue({
-            InvoiceNo:  response['ResponseInvoiceHXObj'].InvoiceNo,
-            InvoiceDt: formatDate(response['ResponseInvoiceHXObj'].InvoiceDt, 'yyyy-MM-dd', 'en-US'),
-            TotalInvoiceAmt: response['ResponseInvoiceHXObj'].TotalInvoiceAmt,
-            InvoiceAmt: response['ResponseInvoiceDXObj'].InvoiceAmt
+            InvoiceNo: response["ResponseInvoiceHXObj"].InvoiceNo,
+            InvoiceDt: formatDate(
+              response["ResponseInvoiceHXObj"].InvoiceDt,
+              "yyyy-MM-dd",
+              "en-US"
+            ),
+            // TotalInvoiceAmt: response["ResponseInvoiceHXObj"].TotalInvoiceAmt,
+            InvoiceAmt: response["ResponseInvoiceDXObj"].InvoiceAmt,
           });
         }
       });
   }
 
-
   async SaveForm() {
-    // this.purchaseOrderHObj.MouNo = this.MouNo;
-    // this.purchaseOrderHObj.Notes = this.Notes;
-    // this.purchaseOrderHObj.PurchaseOrderDt = new Date(formatDate(context[CommonConstant.BUSINESS_DT], 'yyyy-MM-dd', 'en-US'));
-    // this.purchaseOrderHObj.PurchaseOrderExpiredDt = this.PurchaseOrderExpiredDt;
+    if (
+      this.InvoiceDataForm.get("TotalInvoiceAmt").value ==
+      this.purchaseOrderHObj.TotalPurchaseOrderAmt
+    ) {
+      // this.purchaseOrderHObj.MouNo = this.MouNo;
+      // this.purchaseOrderHObj.Notes = this.Notes;
+      // this.purchaseOrderHObj.PurchaseOrderDt = new Date(formatDate(context[CommonConstant.BUSINESS_DT], 'yyyy-MM-dd', 'en-US'));
+      // this.purchaseOrderHObj.PurchaseOrderExpiredDt = this.PurchaseOrderExpiredDt;
 
-    // if (this.bankVisible == true) {
-    //   this.purchaseOrderHObj.BankCode = this.responseRefBank.BankCode;
-    //   this.purchaseOrderHObj.BankBranch = this.responseRefBank.OfficeBankAccBranch;
-    //   this.purchaseOrderHObj.BankAccNo = this.responseRefBank.OfficeBankAccNo;
-    //   this.purchaseOrderHObj.BankAccName = this.responseRefBank.OfficeBankAccName;
-    // }
+      // if (this.bankVisible == true) {
+      //   this.purchaseOrderHObj.BankCode = this.responseRefBank.BankCode;
+      //   this.purchaseOrderHObj.BankBranch = this.responseRefBank.OfficeBankAccBranch;
+      //   this.purchaseOrderHObj.BankAccNo = this.responseRefBank.OfficeBankAccNo;
+      //   this.purchaseOrderHObj.BankAccName = this.responseRefBank.OfficeBankAccName;
+      // }
 
-    // var ListPORefMasterObj = await this.GetFromRule();
-    // var listPurchaseOrderD = this.GenerateRequestPurchaseOrderDObjs(ListPORefMasterObj);
+      // var ListPORefMasterObj = await this.GetFromRule();
+      // var listPurchaseOrderD = this.GenerateRequestPurchaseOrderDObjs(ListPORefMasterObj);
 
+      var RequestInvoiceX = {
+        RequestInvoiceHXObj: {
+          VendorCode: this.InvoiceDataForm.controls.SupplCode.value,
+          InvoiceNo: this.InvoiceDataForm.controls.InvoiceNo.value,
+          InvoiceDt: this.InvoiceDataForm.controls.InvoiceDt.value,
+          RcvBy: this.InvoiceDataForm.controls.RcvBy.value,
+          TotalInvoiceAmt: this.InvoiceDataForm.controls.TotalInvoiceAmt.value,
+        },
+        RequestInvoiceDXObj: {
+          PurchaseOrderHId:
+            this.InvoiceDataForm.controls.PurchaseOrderHId.value,
+          InvoiceAmt: this.InvoiceDataForm.controls.InvoiceAmt.value,
+        },
+      };
 
-
-    var RequestInvoiceX = {
-      RequestInvoiceHXObj: {
-        VendorCode: this.InvoiceDataForm.controls.SupplCode.value,
-        InvoiceNo: this.InvoiceDataForm.controls.InvoiceNo.value,
-        InvoiceDt: this.InvoiceDataForm.controls.InvoiceDt.value,
-        RcvBy: this.InvoiceDataForm.controls.RcvBy.value,
-        TotalInvoiceAmt: this.InvoiceDataForm.controls.TotalInvoiceAmt.value,
-      },
-      RequestInvoiceDXObj: {
-        PurchaseOrderHId: this.InvoiceDataForm.controls.PurchaseOrderHId.value,
-        InvoiceAmt: this.InvoiceDataForm.controls.InvoiceAmt.value,
-      }
+      // var RequestInvoiceX = {
+      //     VendorCode: this.InvoiceDataForm.controls.SupplCode.value,
+      //     InvoiceNo: this.InvoiceDataForm.controls.InvoiceNo.value,
+      //     InvoiceDt: this.InvoiceDataForm.controls.InvoiceDt.value,
+      //     RcvBy: this.InvoiceDataForm.controls.RcvBy.value,
+      //     TotalInvoiceAmt: this.InvoiceDataForm.controls.TotalInvoiceAmt.value,
+      //     PurchaseOrderHId: this.InvoiceDataForm.controls.PurchaseOrderHId.value,
+      //     InvoiceAmt: this.InvoiceDataForm.controls.InvoiceAmt.value,
+      // }
+      console.log(RequestInvoiceX);
+      console.log(this.InvoiceDataForm);
+      this.AddEditInvoice(RequestInvoiceX);
+    } else {
+      this.toastr.warningMessage(
+        "Total Invoice Amount is not equal to Invoice Amount."
+      );
     }
-
-    // var RequestInvoiceX = {
-    //     VendorCode: this.InvoiceDataForm.controls.SupplCode.value,
-    //     InvoiceNo: this.InvoiceDataForm.controls.InvoiceNo.value,
-    //     InvoiceDt: this.InvoiceDataForm.controls.InvoiceDt.value,
-    //     RcvBy: this.InvoiceDataForm.controls.RcvBy.value,
-    //     TotalInvoiceAmt: this.InvoiceDataForm.controls.TotalInvoiceAmt.value,
-    //     PurchaseOrderHId: this.InvoiceDataForm.controls.PurchaseOrderHId.value,
-    //     InvoiceAmt: this.InvoiceDataForm.controls.InvoiceAmt.value,
-    // }
-    console.log(RequestInvoiceX);
-    console.log(this.InvoiceDataForm);
-    this.AddEditInvoice(RequestInvoiceX);
   }
 
   // checkValidExpDt() {
@@ -177,17 +203,36 @@ export class InvoiceDataDetailXComponent implements OnInit {
   async AddEditInvoice(RequestInvoiceX: any) {
     // this.checkValidExpDt();
     if (!this.isDataExist) {
-      this.http.post(URLConstantX.AddInvoiceX, RequestInvoiceX).subscribe(
-        (response) => {
+      this.http
+        .post(URLConstantX.AddInvoiceX, RequestInvoiceX)
+        .subscribe((response) => {
           this.toastr.successMessage(response["message"]);
-          AdInsHelper.RedirectUrl(this.router, [NavigationConstant.NAP_ADM_PRCS_INVOICE_DATA], { "AgrmntId": this.AgrmntId, "AppId": this.AppId, "TaskListId": this.TaskListId, "BizTemplateCode": this.BizTemplateCode });
-
+          AdInsHelper.RedirectUrl(
+            this.router,
+            [NavigationConstant.NAP_ADM_PRCS_INVOICE_DATA],
+            {
+              AgrmntId: this.AgrmntId,
+              AppId: this.AppId,
+              TaskListId: this.TaskListId,
+              BizTemplateCode: this.BizTemplateCode,
+            }
+          );
         });
     } else {
-      this.http.post(URLConstantX.EditInvoiceX, RequestInvoiceX).subscribe(
-        (response) => {
+      this.http
+        .post(URLConstantX.EditInvoiceX, RequestInvoiceX)
+        .subscribe((response) => {
           this.toastr.successMessage(response["message"]);
-          AdInsHelper.RedirectUrl(this.router, [NavigationConstant.NAP_ADM_PRCS_INVOICE_DATA], { "AgrmntId": this.AgrmntId, "AppId": this.AppId, "TaskListId": this.TaskListId, "BizTemplateCode": this.BizTemplateCode });
+          AdInsHelper.RedirectUrl(
+            this.router,
+            [NavigationConstant.NAP_ADM_PRCS_INVOICE_DATA],
+            {
+              AgrmntId: this.AgrmntId,
+              AppId: this.AppId,
+              TaskListId: this.TaskListId,
+              BizTemplateCode: this.BizTemplateCode,
+            }
+          );
         });
     }
   }
