f15905df0c3f3b1701e54a226a366fa7278df682
ramon.akbar <ramon.akbar@ad-ins.com>
2022-08-03 09:40:10 +0700

    Tambahan Validasi SLIK pada NAP 1

--- src/app/impl/shared/constant/ExceptionConstantX.ts
+++ src/app/impl/shared/constant/ExceptionConstantX.ts
@@ -3,7 +3,8 @@ export class ExceptionConstantX {
   public static PLEASE_COMPLETE_DATA_CUSTOMER = "Please Complete Data Customer";
   public static CUST_MUST_HAVE_AGRMNT_PARENT = "Customer must have Agreement parent";
   public static EFF_DATE_MUST_LOWER_THAN_DEL_DT = "Effective Date must not be lower than Delivery Date";
-  public static FINANCING_AMT_CANNOT_LESS_THAN_ZERO = "Financing Amount can not be less than 0"; 
-  public static PLEASE_CLICK_SAVE_FIRST = "Please click Save first"; 
+  public static FINANCING_AMT_CANNOT_LESS_THAN_ZERO = "Financing Amount can not be less than 0";
+  public static PLEASE_CLICK_SAVE_FIRST = "Please click Save first";
   public static IS_OWNER_NEED_SHARE_PRCNT = "Owner Need to Input Share Prcnt";
+  public static IS_NON_OWNER_NEED_NOT_HAVE_SHARE_PRCNT = "Non Owner Need to Input 0% Share";
 }
