f01014a81b7e934515e023c2fd1c1d709179c86c
ramon.akbar <ramon.akbar@ad-ins.com>
2022-08-04 16:30:58 +0700

    Fix NAP 1 shareholder company bug
    (cherry picked from commit b9e15bab28f0bbc5f20c613b03652c61d63954c0)

--- src/app/impl/NEW-NAP/sharing-component/main-data-component/cust-main-data/cust-main-data-x.component.ts
+++ src/app/impl/NEW-NAP/sharing-component/main-data-component/cust-main-data/cust-main-data-x.component.ts
@@ -1639,12 +1639,12 @@ export class CustMainDataXComponent implements OnInit {
       //   return;
       // }
       this.setDataCustomerCompanyForSave();
-      if (this.custDataPersonalObj.AppCustObj.IsShareholder && this.custDataPersonalObj.AppCustCompanyMgmntShrholderObj.IsOwner && this.custDataPersonalObj.AppCustCompanyMgmntShrholderObj.SharePrcnt < 0.0001) {
+      if (this.custDataCompanyObj.AppCustObj.IsShareholder && this.custDataCompanyObj.AppCustCompanyMgmntShrholderObj.IsOwner && this.custDataCompanyObj.AppCustCompanyMgmntShrholderObj.SharePrcnt < 0.0001) {
         this.toastr.warningMessage(String.Format(ExceptionConstantX.IS_OWNER_NEED_SHARE_PRCNT));
         return false;
       }
 
-      if (this.custDataPersonalObj.AppCustObj.IsShareholder && !this.custDataPersonalObj.AppCustCompanyMgmntShrholderObj.IsOwner && this.custDataPersonalObj.AppCustCompanyMgmntShrholderObj.SharePrcnt > 0.0000) {
+      if (this.custDataCompanyObj.AppCustObj.IsShareholder && !this.custDataCompanyObj.AppCustCompanyMgmntShrholderObj.IsOwner && this.custDataCompanyObj.AppCustCompanyMgmntShrholderObj.SharePrcnt > 0.0000) {
         this.toastr.warningMessage(String.Format(ExceptionConstantX.IS_NON_OWNER_NEED_NOT_HAVE_SHARE_PRCNT));
         return false;
       }
