b6f8acfb97f64793f034b5a0d057f159599307b0
Erwin Purnama <erwin.purnama@ad-ins.com>
2022-09-21 11:25:30 +0700

    DSL-130 untuk info agreement cancel yang salah
    (cherry picked from commit cd16b8e65db806a07827257c61156598ab1af451)

--- src/app/NEW-NAP/business-process/admin-process/application-agreement-cancellation/application-agreement-cancellation-detail/application-agreement-cancellation-detail.component.ts
+++ src/app/NEW-NAP/business-process/admin-process/application-agreement-cancellation/application-agreement-cancellation-detail/application-agreement-cancellation-detail.component.ts
@@ -48,6 +48,7 @@ export class ApplicationAgreementCancellationDetailComponent implements OnInit {
 
   ngOnInit() {
     this.arrValue.push(this.AppId);
+    this.arrValue.push(this.AgrmntId);
     this.viewGenericObj.viewInput = "./assets/ucviewgeneric/viewApplicationAgreementCancellation.json";
     this.viewGenericObj.whereValue = this.arrValue;
 
