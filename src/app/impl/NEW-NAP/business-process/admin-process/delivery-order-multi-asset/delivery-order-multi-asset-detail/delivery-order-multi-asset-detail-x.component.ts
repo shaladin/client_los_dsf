d03d967ca6de021182e27a06d5d25581305e6904
ramon.akbar <ramon.akbar@ad-ins.com>
2022-11-21 10:23:37 +0700

    update warning message
    (cherry picked from commit ba26a6e06622c7b7547a974884b2d8f80f7407ff)

--- src/app/impl/NEW-NAP/business-process/admin-process/delivery-order-multi-asset/delivery-order-multi-asset-detail/delivery-order-multi-asset-detail-x.component.ts
+++ src/app/impl/NEW-NAP/business-process/admin-process/delivery-order-multi-asset/delivery-order-multi-asset-detail/delivery-order-multi-asset-detail-x.component.ts
@@ -482,11 +482,10 @@ export class DeliveryOrderMultiAssetDetailXComponent implements OnInit {
     let diffDays = 0;
     const diffTimes = new Date(this.DOAssetForm.controls.EffectiveDt.value).getTime() - new Date(this.DOAssetForm.controls.GoLiveEstimated.value).getTime();
     if (diffTimes > 0) {
-      diffDays = diffTimes / (1000 * 3600 * 24)
-      console.log(diffDays);
+      diffDays = diffTimes / (1000 * 3600 * 24);
     }
     if(this.diffDays > this.maxDiff){
-      this.toastr.warningMessage('Difference date between effective date and go live date is too long');
+      this.toastr.warningMessage('Difference date between effective date and go live date cannot be more than ' + this.maxDiff + ' days');
       return;
     }
     if (this.AppTcForm.valid && this.DOAssetForm.valid) {
