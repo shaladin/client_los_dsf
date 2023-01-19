6ce85c28bb369f801320ce40a28515ea094a2aa6
ramon.akbar <ramon.akbar@ad-ins.com>
2023-01-19 20:09:17 +0700

    add tab history for change mou
    (cherry picked from commit c6029f45af826ba5273eea5b887466a94fb89db8)

---
+++ src/app/MOU/change-mou/change-mou-view/change-mou-detail/change-mou-addcoll/change-mou-addcoll.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NGXToastrService } from 'app/components/extra/toastr/toastr.service';
+import { URLConstant } from 'app/shared/constant/URLConstant';
+import { ResMouCollForMouViewObj } from 'app/shared/model/response/mou/res-mou-coll-for-mou-view-obj.model';
+
+@Component({
+  selector: 'app-change-mou-addcoll',
+  templateUrl: './change-mou-addcoll.component.html'
+})
+export class ChangeMouAddcollComponent implements OnInit {
+  @Input() MouCustId: number;
+  @Input() ChangeMouTrxId: number;
+
+  listCollateralData: Array<ResMouCollForMouViewObj> = new Array();
+
+  constructor(private http: HttpClient, private toastr: NGXToastrService) { }
+
+
+  ngOnInit() {
+    this.http.post(URLConstant.GetChangeMouCustCollateralForChangeMouViewByMouCustId, { Id: this.ChangeMouTrxId }).subscribe(
+      (response) => {
+        console.log(response);
+        this.listCollateralData = response['ReturnObject'];
+      })
+  }
+
+  ChangeMouCustCollateralId: number = 0;
+  isView: boolean = false;
+  ViewColl(ChangeMouCustCollateralId: number) {
+    this.isView = false;
+    setTimeout(() => {
+      this.ChangeMouCustCollateralId = ChangeMouCustCollateralId;
+      this.isView = true;
+    }, 500);
+  }
+
+  Back() {
+    this.isView = false;
+  }
+}
