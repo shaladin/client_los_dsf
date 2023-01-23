7034a565d1944f9a5013a5bc7ee533c408e1d0c3
ramon.akbar <ramon.akbar@ad-ins.com>
2023-01-23 10:31:28 +0700

    Revert "add main cvg on tab insurance"
    This reverts commit 8a322e56f4c4430148c509aee2901defb98a00e1.

--- src/app/impl/view-enhancing/agr-view/view-insurance-data/view-insurance-data-x.component.ts
+++ src/app/impl/view-enhancing/agr-view/view-insurance-data/view-insurance-data-x.component.ts
@@ -3,7 +3,6 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { URLConstantX } from 'app/impl/shared/constant/URLConstantX';
 import { CommonConstant } from 'app/shared/constant/CommonConstant';
-import { URLConstant } from 'app/shared/constant/URLConstant';
 import { ViewInsuranceDataDetailXComponent } from './view-insurance-data-detail-x/view-insurance-data-detail-x.component';
 
 @Component({
@@ -18,16 +17,13 @@ export class ViewInsuranceDataXComponent implements OnInit {
   totalCustPaidAmt: number = 0;
   totalCapitalizedAmt: number = 0;
   totalCustDiscAmt: number = 0;
-  appCollObjFinal: Array<any>;
-  appInsCvgs: any;
 
   constructor(private http: HttpClient, private modalService: NgbModal) { }
 
-  async ngOnInit() {
-    await this.http.post(URLConstantX.GetListAppInsObjByAgrmntIdForViewX, {Id: this.AgrmntId}).toPromise().then(
+  ngOnInit() {
+    this.http.post(URLConstantX.GetListAppInsObjByAgrmntIdForViewX, {Id: this.AgrmntId}).subscribe(
       (response) => {
         this.appCollObjs = response[CommonConstant.ReturnObj];
-        this.appCollObjFinal = this.appCollObjs
 
         for(let i = 0; i < this.appCollObjs.length; i++) {
           if(this.appCollObjs[i].TotalInsCustAmt != null) {
@@ -35,7 +31,7 @@ export class ViewInsuranceDataXComponent implements OnInit {
           }
 
           if(this.appCollObjs[i].TotalCustDiscAmt != null) {
-            this.totalCustDiscAmt += this.appCollObjs[i].TotalCustDiscAmt;
+            this.totalCustDiscAmt += this.appCollObjs[i].TotalCustDiscAmt; 
           }
 
           if(this.appCollObjs[i].InsCpltzAmt != null) {
@@ -45,24 +41,9 @@ export class ViewInsuranceDataXComponent implements OnInit {
 
         this.custTotalPremi -= this.totalCustDiscAmt;
         this.totalCustPaidAmt = this.custTotalPremi - this.totalCapitalizedAmt;
+        
       }
     );
-    for (let i = 0; i < this.appCollObjFinal.length; i++){
-      await this.http.post(URLConstant.GetAppInsObjViewDetail, { Id: this.appCollObjFinal[i].AppInsObjId }).toPromise().then(
-        (response: any) => {
-          this.appInsCvgs = response.appInsCvgs;
-          let list = [];
-          let joinText: any;
-          list = this.appInsCvgs.map(function(val){
-            return val.appInsMainCvgObj.MrMainCvgTypeCode;
-          });
-          list = list.filter(function (x, i, a) {
-            return a.indexOf(x) == i;
-          });
-          joinText = list.join(',');
-          this.appCollObjFinal[i].MrMainCvgTypeCode = joinText;
-        });
-      }
   }
 
   viewDetailCollateralHandler(appInsObjId){
