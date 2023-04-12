d617a522ef5ad23b4286af7d51ad948f6bf29135
Felix Nugraha <felix.nugraha@ad-ins.com>
2023-04-12 16:41:40 +0700

    Rework 1.5.14

--- src/app/impl/NEW-NAP/business-process/credit-process/commission-reserved-fund/comission-reserved-fund-paging-x/comission-reserved-fund-paging-x.component.ts
+++ src/app/impl/NEW-NAP/business-process/credit-process/commission-reserved-fund/comission-reserved-fund-paging-x/comission-reserved-fund-paging-x.component.ts
@@ -290,71 +290,8 @@ export class ComissionReservedFundPagingXComponent implements OnInit {
         this.toastr.warningMessage("Please contact " + ev.RowObj.LastUserInput + " (" + this.lastUserInput.EmpName +") to edit this application");
         return;
       }
-      if (!await this.CheckRuleObj(ev.RowObj.AppId)) {
-        this.toastr.warningMessage("Please complete MaxRefund Rule for " + this.BizTemplateCode + " ");
-        return;
-      }
       AdInsHelper.RedirectUrl(this.router, [NavigationConstant.NAP_CRD_PRCS_COMM_RSV_FUND_DETAIL], { "AppId": ev.RowObj.AppId, "WfTaskListId": environment.isCore ? ev.RowObj.Id : ev.RowObj.WfTaskListId });      
     }
-  }
-
-  DictMaxRefundChecked: { [id: string]: boolean } = {};
-  async CheckRuleObj(appId: number): Promise<boolean> {
-    this.DictMaxRefundChecked = {};
-    let flag = true;
-    let getRuleCommission = await this.http.post(URLConstant.GetAppCommissionRule, { AppId: appId, BizTemplateCode: this.BizTemplateCode });
-    let getRuleRsvFund = await this.http.post(URLConstant.CreateRsvFundRule, { Id: appId });
-    let getMaxRefund = await this.http.post<AppFinDataObj>(URLConstant.GetAppFinDataWithRuleByAppId, { Id: appId });
-    await forkJoin([getRuleCommission, getRuleRsvFund, getMaxRefund]).toPromise().then(
-      (response) => {
-        let TempResRuleCommissionObj = response[0][CommonConstant.ReturnObj];
-        let TempResRuleRsvFundObj = response[1][CommonConstant.ReturnObj];
-        let TempListResultRefundObj: Array<ResultRefundObj> = response[2]["ResultRefundRsvFundObjs"];
-
-        for (let index = 0; index < TempResRuleCommissionObj.length; index++) {
-          const element = TempResRuleCommissionObj[index][CommonConstant.ReturnObj]["RuleDataObjects"];
-
-          if (element.ResultSupplier != null && element.ResultSupplier != undefined) {
-            flag = this.CheckRuleCompletion(element.ResultSupplier.AllocationFrom, TempListResultRefundObj, flag);
-          }
-          if (element.ResultSupplierEmp != null && element.ResultSupplierEmp != undefined) {
-            flag = this.CheckRuleCompletion(element.ResultSupplierEmp.AllocationFrom, TempListResultRefundObj, flag);
-          }
-          if (element.ResultReferantor != null && element.ResultReferantor != undefined) {
-            flag = this.CheckRuleCompletion(element.ResultReferantor.AllocationFrom, TempListResultRefundObj, flag);
-          }
-        }
-
-        for (let index = 0; index < TempResRuleRsvFundObj.length; index++) {
-          const element = TempResRuleRsvFundObj[index].AllocationFrom;
-          flag = this.CheckDictMaxRefundChecked(element, TempListResultRefundObj, flag);
-        }
-      }
-    );
-    return flag;
-  }
-
-  CheckRuleCompletion(listAllocFrom: Array<string>, listResultRefund: Array<ResultRefundObj>, flag: boolean): boolean {
-    for (let index = 0; index < listAllocFrom.length; index++) {
-      const element: string = listAllocFrom[index];
-      flag = this.CheckDictMaxRefundChecked(element, listResultRefund, flag);
-    }
-    return flag;
-  }
-
-  CheckDictMaxRefundChecked(allocFrom: string, listResultRefund: Array<ResultRefundObj>, flag: boolean): boolean {
-    if (this.DictMaxRefundChecked[allocFrom] != undefined && this.DictMaxRefundChecked[allocFrom]) {
-      return flag;
-    } else {
-      this.DictMaxRefundChecked[allocFrom] = true;
-      let tempIdx: number = listResultRefund.findIndex(x => x.RefundAllocationFrom == allocFrom);
-
-      if (tempIdx == -1) {
-        this.toastr.warningMessage(this.BizTemplateCode + "  does not have " + allocFrom + " in MaxRefund Rule");
-        flag = false;
-      }
-    }
-    return flag;
   }
 
 }
