5c6ff2b125dc3dc7d77762a39725c50a9b1b214e
Wahyu <wahyu@ad-ins.com>
2023-06-15 12:42:45 +0700

    C0025470 CR Improvement Telemarketing Offering LOS FE V1.020

--- src/app/dsf/lead/potential-ro-dsf/generate-potential-ro-monitoring-dsf/generate-potential-ro-monitoring-dsf.component.ts
+++ src/app/dsf/lead/potential-ro-dsf/generate-potential-ro-monitoring-dsf/generate-potential-ro-monitoring-dsf.component.ts
@@ -21,6 +21,8 @@ export class GeneratePotentialRoMonitoringDsfComponent implements OnInit {
     this.uploadObj.TemplateName = "Upload_PotentialRO_Template";
     this.uploadObj.FileErrorName = "Upload_PotentialRO_ErrorDownload";
     this.uploadObj.pagingJson = "./assets/dsf/ucpaging/searchPotentialROMonitoringDsf.json";
+    this.uploadObj.formatsAllowed = ".xls, .xlsx";
+    this.uploadObj.maxSize = 2;
     if (!environment.isCore) {
       this.uploadObj.formatsAllowed = ".xls, .xlsx";
       this.uploadObj.url = URLConstantX.UploadFileV2;
