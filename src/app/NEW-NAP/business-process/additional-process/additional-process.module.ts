7a1af84fe0a1d84baf9012d31d1c915330648db0
Agung Januardi <agung.januardi@bsi.co.id>
2023-12-22 10:01:59 +0700

    Rework Rollback Batch DSL 037 V4

--- src/app/NEW-NAP/business-process/additional-process/additional-process.module.ts
+++ src/app/NEW-NAP/business-process/additional-process/additional-process.module.ts
@@ -92,9 +92,6 @@ import { ReturnHandlingDetailDsfComponent } from "app/NEW-NAP/DSF/business-proce
 import { PlafondInstallmentSimulationPagingDsfComponent } from "app/NEW-NAP/DSF/business-process-dsf/additional-process-dsf/plafond-installment-simulation-paging-dsf/plafond-installment-simulation-paging-dsf.component";
 import { ReturnHandlingEditNap4DsfComponent } from "app/NEW-NAP/DSF/business-process-dsf/additional-process-dsf/return-handling-dsf/return-handling-edit-nap4-dsf/return-handling-edit-nap4-dsf.component";
 import { PlafondInstallmentSimulationDetailDsfComponent } from "app/NEW-NAP/DSF/business-process-dsf/additional-process-dsf/plafond-installment-simulation-detail-dsf/plafond-installment-simulation-detail-dsf.component";
-import { ReturnHandlingCollateralPagingXComponent } from "app/impl/NEW-NAP/business-process/additional-process/return-handling/return-handling-collateral-paging/return-handling-collateral-paging-x.component";
-import { ReturnHandlingCollateralEditXComponent } from "app/impl/NEW-NAP/business-process/additional-process/return-handling/return-handling-collateral-edit/return-handling-collateral-edit-x.component";
-
 
 export const customCurrencyMaskConfig = {
   align: "right",
