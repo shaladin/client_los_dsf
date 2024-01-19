406438d3791ce89080ca9c2824d3248f7937860f
Agung Januardi <agung.januardi@bsi.co.id>
2024-01-19 08:06:49 +0700

    Revert "CR LOS R3 Enhancement of Refinancing V1.2"
    This reverts commit 190445368d9f14a52a6728f96514e3fa169d59e6.

--- src/app/NEW-NAP/DSF/input-nap-component-dsf/application-data-x-dsf/application-data-x-dsf.component.ts
+++ src/app/NEW-NAP/DSF/input-nap-component-dsf/application-data-x-dsf/application-data-x-dsf.component.ts
@@ -281,7 +281,7 @@ export class ApplicationDataXDsfComponent implements OnInit {
   isActiveMode: boolean = false;
   generalSettingPlafondObj: GeneralSettingObj;
   returnGeneralPlafondSettingObj: GeneralSettingObj;
-  isOverMinimumPlafod: boolean = true;
+  isOverMinimumPlafod: boolean = false;
   isOverTenor: boolean = false;
   businessDt: any;
   //End Self Custom CR MPF & FD Validation
@@ -1338,11 +1338,17 @@ export class ApplicationDataXDsfComponent implements OnInit {
           await this.http.post(URLConstant.GetGeneralSettingByCode, obj).toPromise().then(
             (response: GeneralSettingObj) => {
               this.returnGeneralPlafondSettingObj = response;
-              if (parseInt(this.returnGeneralPlafondSettingObj.GsValue) >= this.MaxPlafondMasterAgreement)
+              if (Number(this.returnGeneralPlafondSettingObj.GsValue) <= this.MaxPlafondMasterAgreement)
               {
-                this.isOverMinimumPlafod = false;
+                this.isOverMinimumPlafod = true;
               }
           });
+
+          if (!this.isOverMinimumPlafod)
+          {
+            this.toastr.warningMessage(ExceptionConstantDsf.VALIDATE_MIN_PLAFOND);
+            return false;
+          }
 
           let runningTenor = this.monthDiff(this.agrParent.AgrmntDt);
           this.generalSettingPlafondObj = new GeneralSettingObj();
@@ -1358,6 +1364,12 @@ export class ApplicationDataXDsfComponent implements OnInit {
                 this.isTenorValid = true;
               }
           });
+
+          if (!this.isTenorValid)
+          {
+            this.toastr.warningMessage(ExceptionConstantDsf.VALIDATE_TENOR_LIMIT);
+            return false;
+          }
     }
     // End Self Custom CR MPF & FD Validation
 
@@ -1549,18 +1561,6 @@ export class ApplicationDataXDsfComponent implements OnInit {
     if (this.BizTemplateCode == CommonConstant.CFNA) {
       // Self Custom CR MPF & FD Validation
       await this.validateRequestedPlafond();
-      if (!this.isOverMinimumPlafod)
-      {
-        this.toastr.warningMessage(ExceptionConstantDsf.VALIDATE_MIN_PLAFOND);
-        return false;
-      }
-
-      if (!this.isTenorValid)
-      {
-        this.toastr.warningMessage(ExceptionConstantDsf.VALIDATE_TENOR_LIMIT);
-        return false;
-      }
-
       if (this.IsRequestedPlafondExceed)
       {
         this.toastr.warningMessage(ExceptionConstantDsf.VALIDATE_REQUESTED_PLAFOND);
@@ -2505,8 +2505,8 @@ export class ApplicationDataXDsfComponent implements OnInit {
   monthDiff(agrmntDate: Date)
   {
     this.businessDt = new Date(AdInsHelper.GetCookie(this.cookieService, CommonConstant.BUSINESS_DATE_RAW));
-    const monthDiff = this.businessDt.getMonth() - new Date(agrmntDate).getMonth();
-    const yearDiff = this.businessDt.getFullYear() - new Date(agrmntDate).getFullYear();
+    const monthDiff = this.businessDt.getMonth() - agrmntDate.getMonth();
+    const yearDiff = this.businessDt.getFullYear() - agrmntDate.getFullYear();
 
     return monthDiff + yearDiff * 12;
   }
