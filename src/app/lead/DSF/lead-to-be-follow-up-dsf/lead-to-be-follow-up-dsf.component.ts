e4f2f307dac13bda8cc2d6be62deaa3966bd2b1a
agung.januardi <agung.januardi@adins.com>
2024-04-21 13:47:19 +0700

    CR Simple Lead V1.0

--- src/app/lead/DSF/lead-to-be-follow-up-dsf/lead-to-be-follow-up-dsf.component.ts
+++ src/app/lead/DSF/lead-to-be-follow-up-dsf/lead-to-be-follow-up-dsf.component.ts
@@ -1,11 +1,14 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { AdInsHelper } from 'app/shared/AdInsHelper';
+import { CommonConstant } from 'app/shared/constant/CommonConstant';
 import { URLConstant } from 'app/shared/constant/URLConstant';
 import { URLConstantDsf } from 'app/shared/constant/URLConstantDsf';
 import { GenericObj } from 'app/shared/model/generic/generic-obj.model';
 import { UcPagingObj } from 'app/shared/model/uc-paging-obj.model';
+import { find } from 'core-js/core/array';
 import { environment } from 'environments/environment';
+import { CookieService } from 'ngx-cookie';
 
 @Component({
   selector: 'app-lead-to-be-follow-up-dsf',
@@ -17,14 +20,18 @@ export class LeadToBeFollowUpDsfComponent implements OnInit {
   ReqByIdObj: GenericObj = new GenericObj();
   inputPagingObj: UcPagingObj = new UcPagingObj();
   CustId: string;
+  IsReady: boolean = false;
+  CurrentUserContext: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private elRef: ElementRef, private renderer: Renderer2, private cookieService: CookieService) { }
 
   ngOnInit() {
 
     // Self Custom Changes:
+    this.CurrentUserContext = JSON.parse(AdInsHelper.GetCookie(this.cookieService, CommonConstant.USER_ACCESS));
     this.inputPagingObj._url = "./assets/dsf/ucpaging/searchNewLeadToBeFollowUpDsf.json";
     this.inputPagingObj.pagingJson = "./assets/dsf/ucpaging/searchNewLeadToBeFollowUpDsf.json";
+    this.IsReady = true;
     // End of Self Custom Changes
 
     this.inputPagingObj.ddlEnvironments = [
@@ -35,9 +42,32 @@ export class LeadToBeFollowUpDsfComponent implements OnInit {
       {
         name: "L.MR_LEAD_SOURCE_CODE",
         environment: environment.FoundationR3Url + "/v1"
+      },
+      {
+        name: "LC.MR_CUST_TYPE_CODE",
+        environment: environment.FoundationR3Url + "/v1"
       }
     ];
   }
+
+  // Self Custom Changes
+  ngAfterViewInit(){
+    if (this.IsReady)
+      {
+        if (this.CurrentUserContext.RoleCode == "DPC" && this.CurrentUserContext.OfficeCode == "1000")
+        {
+          setTimeout(() => {
+            const element1 = this.elRef.nativeElement.querySelector('select[data-name="L.ORI_OFFICE_CODE"]');
+            if (element1) {
+              element1.value = this.CurrentUserContext.OfficeCode;
+              this.renderer.setProperty(element1, "hidden", true);
+              element1.insertAdjacentHTML('afterend', '<label>' + this.CurrentUserContext.OfficeName + '</label>');
+            }
+          }, 500);
+        }
+      }
+  }
+  // Self Custom Changes
 
   GetCallBack(ev: any) {
     if (ev.Key == "Edit") {
