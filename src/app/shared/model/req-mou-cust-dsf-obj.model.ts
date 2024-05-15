23677c92f6928421f38ce1a0cd0259939ad973ab
agung.januardi <agung.januardi@adins.com>
2024-05-15 22:18:06 +0700

    CR Vendor Grading Factoring V1.26

--- src/app/shared/model/req-mou-cust-dsf-obj.model.ts
+++ src/app/shared/model/req-mou-cust-dsf-obj.model.ts
@@ -1,12 +1,10 @@
 export class RequestMouCustDsfObj {
     MouCustId: number;
     ChangeMouCustId: number;
-    IsReview: boolean;
     RowVersion: string;
 
     constructor() { 
         this.MouCustId = 0;
         this.ChangeMouCustId = 0;
-        this.IsReview = false;
         this.RowVersion = ""}
 }
